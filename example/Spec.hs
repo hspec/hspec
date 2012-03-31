bb19533f67dcb843fba242fc93fb6d7e46b3eca1
Simon Hengel <sol@typeful.net>
2012-03-31 11:26:48 +0200

    Add example spec

---
+++ example/Spec.hs
@@ -0,0 +1,22 @@
+module Spec (main, spec) where
+
+import Test.Hspec.Monadic
+import Test.Hspec.HUnit ()
+import Test.Hspec.QuickCheck
+import Test.HUnit
+
+shouldBe :: (Show a, Eq a) => a -> a -> Assertion
+actual `shouldBe` expected = actual @?= expected
+
+main :: IO ()
+main = hspecX spec
+
+spec :: Specs
+spec = do
+
+  describe "reverse" $ do
+    it "reverses a list" $ do
+      reverse [1 :: Int, 2, 3] `shouldBe` [3, 2, 1]
+
+    it "gives the original list, if applied twice" $ property $
+      \(xs) -> reverse (reverse xs) == (xs :: [Int])
