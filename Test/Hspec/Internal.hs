8192726d28159ba943c3ec52b0078d3ff8fa2bb5
Simon Hengel <sol@typeful.net>
2012-05-06 08:43:50 +0200

    Add more documentation

--- Test/Hspec/Internal.hs
+++ Test/Hspec/Internal.hs
@@ -77,6 +77,21 @@ instance Example Result where
 
 newtype Pending = Pending (Maybe String)
 
+-- | A pending example.
+--
+-- If you want to report on a behavior but don't have an example yet, use this.
+--
+-- > describe "fancyFormatter" [
+-- >   it "can format text in a way that everyone likes" $
+-- >     pending
+-- > ]
+--
+-- You can give an optional reason for why it's pending.
+--
+-- > describe "fancyFormatter" [
+-- >   it "can format text in a way that everyone likes" $
+-- >     pending "waiting for clarification from the designers"
+-- > ]
 pending :: String -> Pending
 pending = Pending . Just
 
--- Test/Hspec/Monadic.hs
+++ Test/Hspec/Monadic.hs
@@ -142,13 +142,13 @@ it label action = (SpecM . tell) [Core.it label action]
 -- If you want to report on a behavior but don't have an example yet, use this.
 --
 -- > describe "fancyFormatter" $ do
--- >   it "can format text in a way that everyone likes" $ do
+-- >   it "can format text in a way that everyone likes" $
 -- >     pending
 --
--- You can give an reason for why it's pending.
+-- You can give an optional reason for why it's pending.
 --
 -- > describe "fancyFormatter" $ do
--- >   it "can format text in a way that everyone likes" $ do
+-- >   it "can format text in a way that everyone likes" $
 -- >     pending "waiting for clarification from the designers"
 pending :: String  -> Pending
 pending = Core.pending
