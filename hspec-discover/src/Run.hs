fef97443c971f00e5cfe18f4013839f8b27a684d
Simon Hengel <sol@typeful.net>
2012-05-13 10:15:42 +0200

    hspec-discover: Sort specs

--- hspec-discover/src/Run.hs
+++ hspec-discover/src/Run.hs
@@ -6,16 +6,13 @@ import           Data.List
 import           System.Directory
 import           System.FilePath
 
-data SpecTree = SpecNode {
-  specNodeName      :: String
-, specNodeInhabited :: Bool
-, specNodeChildren  :: [SpecTree]
-} deriving (Eq, Show)
+data SpecTree = SpecNode String Bool [SpecTree]
+  deriving (Eq, Show)
 
 findSpecs :: FilePath -> IO [SpecTree]
 findSpecs dir = do
   c <- getDirectoryContents dir
-  specs <- filterSpecs <$> filterM (doesFileExist . (dir </>)) c
+  specs <- sort . filterSpecs <$> filterM (doesFileExist . (dir </>)) c
   return $ map (\x -> SpecNode x True []) specs
   where
     filterSpecs = map stripSuffix . filter (isSuffixOf suffix)
--- hspec-discover/test/RunSpec.hs
+++ hspec-discover/test/RunSpec.hs
@@ -11,5 +11,11 @@ main = hspecX spec
 spec :: Specs
 spec = do
   describe "findSpecs" $ do
-    it "finds a single spec" $ do
-      findSpecs "test-data/single-spec" `shouldReturn` [SpecNode "Foo" True []]
+    context "when specs are not nested" $ do
+      it "finds a single spec" $ do
+        findSpecs "test-data/single-spec" `shouldReturn` [SpecNode "Foo" True []]
+
+      it "finds several specs" $ do
+        findSpecs "test-data/several-specs" `shouldReturn` [SpecNode "Bar" True [], SpecNode "Baz" True [], SpecNode "Foo" True []]
+  where
+    context = describe
