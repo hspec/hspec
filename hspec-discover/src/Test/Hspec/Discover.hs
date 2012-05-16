d6d837f3ed0badd1500faf5459d6fbf80005390e
Simon Hengel <sol@typeful.net>
2012-05-16 08:52:59 +0200

    hspec-discover: Use prune instead of hide for documentation
    (Hackage does not properly handle hide)

--- hspec-discover/src/Test/Hspec/Discover.hs
+++ hspec-discover/src/Test/Hspec/Discover.hs
@@ -1,7 +1,37 @@
-{-# OPTIONS_HADDOCK hide #-}
+{-# OPTIONS_HADDOCK prune #-}
+-- |
+-- Say you have a driver module for your test suite.
+--
+-- > module Main where
+-- >
+-- > import Test.Hspec.Monadic
+-- >
+-- > import qualified FooSpec
+-- > import qualified Foo.BarSpec
+-- > import qualified BazSpec
+-- >
+-- > main :: IO ()
+-- > main = hspecX $ do
+-- >   describe "Foo"     FooSpec.spec
+-- >   describe "Foo.Bar" Foo.BarSpec.spec
+-- >   describe "Baz"     BazSpec.spec
+--
+-- Then you can replace it with the following.
+--
+-- > {-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+--
+-- All files with a name that ends in @Spec.hs@ are include in the generated
+-- test suite.  And it is assumed, that they export a @spec@ of type
+-- `Test.Hspec.Monadic.Specs`.
+--
+-- Full documentation is here: <https://github.com/sol/hspec-discover#readme>
 module Test.Hspec.Discover (hspec, describe) where
 
-import Test.Hspec.Monadic (Specs, hspecX, describe)
+import           Test.Hspec.Monadic (Specs)
+import qualified Test.Hspec.Monadic as Hspec
 
 hspec :: Specs -> IO ()
-hspec = hspecX
+hspec = Hspec.hspecX
+
+describe :: String -> Specs -> Specs
+describe = Hspec.describe
