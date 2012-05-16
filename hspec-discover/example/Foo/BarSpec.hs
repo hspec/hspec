fe161b6be8e41bccde0498ac2a4b04875a49c11f
Simon Hengel <sol@typeful.net>
2012-05-16 10:39:46 +0200

    hspec-discover: Add an example

---
+++ hspec-discover/example/Foo/BarSpec.hs
@@ -0,0 +1,16 @@
+module Foo.BarSpec (main, spec) where
+
+import           Test.Hspec.ShouldBe
+
+main :: IO ()
+main = hspecX spec
+
+spec :: Specs
+spec = do
+
+  describe "reverse" $ do
+    it "reverses a list" $ do
+      reverse [1 :: Int, 2, 3] `shouldBe` [3, 2, 1]
+
+    it "gives the original list, if applied twice" $ property $
+      \xs -> reverse (reverse xs) == (xs :: [Int])
