d9445b3a4813c4dbf498d2ba839a60f0753ded82
Simon Hengel <sol@typeful.net>
2012-06-09 17:01:12 +0200

    Move specs for `it` and `describe` to test/Test/Hspec/CoreSpec.hs

--- Specs.hs
+++ Specs.hs
@@ -1,14 +1,13 @@
 {-# OPTIONS_GHC -fno-warn-deprecations #-}
 module Main (main) where
 
-import           Test.Hspec.ShouldBe (Specs, describe, it, hspecX, shouldBe, shouldSatisfy)
+import           Test.Hspec.ShouldBe (Specs, describe, it, hspecX, shouldBe)
 
 import qualified Test.Hspec as H
 import           Test.Hspec hiding (Specs, describe, it, hspecX)
-import           Test.Hspec.Internal (SpecTree(..), Spec(..), Result(..))
+import           Test.Hspec.Internal (Result(..))
 import           Test.Hspec.QuickCheck
 import           Test.Hspec.HUnit ()
-import           Test.HUnit
 import           Data.List (isPrefixOf)
 import qualified Test.HUnit as HUnit
 import           Util
@@ -30,46 +29,6 @@ specs = do
   report <- runSpec testSpecs
 
   return $ do
-    describe "the \"describe\" function" $ do
-
-        let testSpec = [
-              H.describe "some subject" [
-                H.it "foo" True
-              , H.it "bar" True
-              , H.it "baz" True
-              ]
-              ]
-
-        it "takes a description of what the behavior is for" $ do
-            r <- runSpec testSpec
-            r `shouldSatisfy` any (== "some subject")
-
-        it "groups behaviors for what's being described" $ do
-            r <- filter (isPrefixOf " - ") `fmap` runSpec testSpec
-            length r `shouldBe` 3
-
-        describe "a nested description" $ do
-            it "has it's own specs"
-                (True)
-
-    describe "the \"it\" function" $ do
-        it "takes a description of a desired behavior" $
-            case unSpec $ H.it "whatever" Success of
-              SpecExample requirement _ -> requirement == "whatever"
-              _ -> False
-
-        it "takes an example of that behavior" $ do
-            case unSpec $ H.it "whatever" Success of
-              SpecExample _ example -> do
-                r <- example
-                r @?= Success
-              SpecGroup _ _ -> assertFailure "unexpected SpecGroup"
-
-        it "can use a Bool, HUnit Test, QuickCheck property, or \"pending\" as an example"
-            (True)
-
-        it "will treat exceptions as failures"
-            (any (==" - exceptions FAILED [3]") report)
 
     describe "the \"hspec\" function" $ do
         it "displays a header for each thing being described"
