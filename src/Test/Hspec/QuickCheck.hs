5a4ca42ec9467a4484c9958687727ef53f52502e
Niklas Hambüchen <mail@nh2.me>
2012-08-15 17:31:22 +0100

    Allow custon QuickCheck examples.
    Example:
        it "..." $ quickCheckExample stdArgs{ maxSuccess = 1000 } myprop

--- src/Test/Hspec/QuickCheck.hs
+++ src/Test/Hspec/QuickCheck.hs
@@ -10,9 +10,13 @@ module Test.Hspec.QuickCheck (
   property
 -- * Shortcuts
 , prop
+, QuickCheckExample
+-- ** QuickCheck customizations
+, quickCheckExample
 ) where
 
 import           Test.QuickCheck
+import           Test.Hspec.Core (Example(..), Params(..))
 import           Test.Hspec
 
 -- |
@@ -25,3 +29,18 @@ import           Test.Hspec
 -- >   ..
 prop :: Testable prop => String -> prop -> Spec
 prop s = it s . property
+
+-- | A custom QuickCheck test to be run by hspec.
+data QuickCheckExample = QuickCheckExample Args Property
+
+-- | Runs a QuickCheck property with custom settings. Overrides `paramsQuickCheckArgs`.
+-- Example:
+--
+-- > it "passes 1000 checks" $
+-- >   quickCheckExample stdArgs{ maxSuccess = 1000 } myprop
+quickCheckExample :: Args -> Property -> QuickCheckExample
+quickCheckExample = QuickCheckExample
+
+instance Example QuickCheckExample where
+  evaluateExample c (QuickCheckExample args p) =
+    evaluateExample c {paramsQuickCheckArgs = args} p
