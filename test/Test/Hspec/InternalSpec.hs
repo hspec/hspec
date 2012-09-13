a8daef0ddff15a9d6bb11d4607e7a6e57729b3de
Simon Hengel <sol@typeful.net>
2012-09-13 14:20:40 +0200

    Add more tests

--- test/Test/Hspec/InternalSpec.hs
+++ test/Test/Hspec/InternalSpec.hs
@@ -5,9 +5,13 @@ import           Test.Hspec.Meta
 import           System.IO.Silently
 import qualified Test.Hspec.Internal as H
 import qualified Test.Hspec.Config as H
+import           Test.QuickCheck
 
 main :: IO ()
 main = hspec spec
+
+evaluateExample :: H.Example e => e -> IO H.Result
+evaluateExample = H.evaluateExample H.defaultConfig
 
 spec :: Spec
 spec = do
@@ -15,23 +19,35 @@ spec = do
 
     context "for Bool" $ do
       it "returns Success on True" $ do
-        H.evaluateExample H.defaultConfig True `shouldReturn` H.Success
+        evaluateExample True `shouldReturn` H.Success
 
       it "returns Fail on False" $ do
-        H.evaluateExample H.defaultConfig False `shouldReturn` H.Fail ""
+        evaluateExample False `shouldReturn` H.Fail ""
 
       it "propagates exceptions" $ do
-        H.evaluateExample H.defaultConfig (error "foobar" :: Bool) `shouldThrow` errorCall "foobar"
+        evaluateExample (error "foobar" :: Bool) `shouldThrow` errorCall "foobar"
 
     context "for Expectation" $ do
       it "returns Success if all expectations hold" $ do
-        H.evaluateExample H.defaultConfig (23 `shouldBe` (23 :: Int)) `shouldReturn` H.Success
+        evaluateExample (23 `shouldBe` (23 :: Int)) `shouldReturn` H.Success
 
       it "returns Fail if an expectation does not hold" $ do
-        H.evaluateExample H.defaultConfig (23 `shouldBe` (42 :: Int)) `shouldReturn` H.Fail "expected: 42\n but got: 23"
+        evaluateExample (23 `shouldBe` (42 :: Int)) `shouldReturn` H.Fail "expected: 42\n but got: 23"
 
       it "propagates exceptions" $ do
-        H.evaluateExample H.defaultConfig (error "foobar" :: Expectation) `shouldThrow` errorCall "foobar"
+        evaluateExample (error "foobar" :: Expectation) `shouldThrow` errorCall "foobar"
 
       it "silences any output to stdout" $ do
-        (capture . H.evaluateExample H.defaultConfig $ putStrLn "foobar") `shouldReturn` ("", H.Success)
+        (capture . evaluateExample $ putStrLn "foobar") `shouldReturn` ("", H.Success)
+
+    context "for Property" $ do
+      it "returns Success if property hold" $ do
+        evaluateExample (property $ \n -> n == (n :: Int)) `shouldReturn` H.Success
+
+      it "returns Fail if property does not hold" $ do
+        H.Fail _ <- evaluateExample $ property $ \n -> n /= (n :: Int)
+        return ()
+
+      it "propagates exceptions" $ do
+        pending "this probaly needs a patch to QuickCheck"
+        -- evaluateExample (property $ (error "foobar" :: Int -> Bool)) `shouldThrow` errorCall "foobar"
