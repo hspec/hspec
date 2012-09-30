0d181cbf4b2e59b2d69be6b78280f11c36b77f73
Simon Hengel <sol@typeful.net>
2012-09-30 11:21:35 +0200

    Use monadic specs for Test.Hspec.RunnerSpec

--- test/Test/Hspec/RunnerSpec.hs
+++ test/Test/Hspec/RunnerSpec.hs
@@ -9,7 +9,8 @@ import           System.Environment
 import           System.Exit
 import qualified Control.Exception as E
 import qualified Test.Hspec.Runner as H
-import qualified Test.Hspec.Core as H
+import qualified Test.Hspec.Monadic as H (describe, it)
+import           Test.Hspec.Monadic (runSpecM)
 import qualified Test.Hspec.Formatters as H
 import           Util
 
@@ -29,14 +30,19 @@ spec = do
 
   describe "hspec" $ do
     it "runs a spec" $ do
-      H.hspec [H.it "foobar" True] `shouldReturn` ()
+      H.hspec . runSpecM $ do
+        H.it "foobar" True
+      `shouldReturn` ()
 
     it "exits with exitFailure if not all examples pass" $ do
-      H.hspec [H.it "foobar" False] `shouldThrow` (== ExitFailure 1)
+      H.hspec . runSpecM $ do
+        H.it "foobar" False
+      `shouldThrow` (== ExitFailure 1)
 
     it "suppresses output to stdout when evaluating examples" $ do
-      r <- capture $
-        H.hspec [H.it "foobar" $ putStrLn "baz"]
+      r <- capture . H.hspec . runSpecM $ do
+        H.it "foobar" $ do
+          putStrLn "baz"
       r `shouldSatisfy` notElem "baz"
 
     context "command-line options" $ do
@@ -49,13 +55,17 @@ spec = do
             ]
 
       it "does not leak command-line flags to examples" $ do
-        withArgs ["--verbose"] $
-          H.hspec [H.it "foobar" $ getArgs `shouldReturn` []] `shouldReturn` ()
+        withArgs ["--verbose"] $ do
+          H.hspec . runSpecM $ do
+            H.it "foobar" $ do
+              getArgs `shouldReturn` []
+          `shouldReturn` ()
 
       describe "option '--verbose'" $ do
         it "does not suppress output to stdout when evaluating examples" $ do
-          r <- capture . withArgs ["--verbose"] $
-            H.hspec [H.it "foobar" $ putStrLn "baz"]
+          r <- capture . withArgs ["--verbose"] .  H.hspec . runSpecM $ do
+            H.it "foobar" $ do
+              putStrLn "baz"
           r `shouldSatisfy` elem "baz"
 
       describe "option '--match'" $ do
@@ -63,55 +73,44 @@ spec = do
           e1 <- newMock
           e2 <- newMock
           e3 <- newMock
-          withArgs ["-m", "/bar/example"] $
-            H.hspec [
-              H.describe "foo" [
-                H.describe "bar" [
-                  H.it "example 1" $ mockAction e1
-                , H.it "example 2" $ mockAction e2
-                ]
-              , H.describe "baz" [
-                  H.it "example 3" $ mockAction e3
-                ]
-              ]
-            ]
+          withArgs ["-m", "/bar/example"] .  H.hspec . runSpecM $ do
+            H.describe "foo" $ do
+              H.describe "bar" $ do
+                H.it "example 1" $ mockAction e1
+                H.it "example 2" $ mockAction e2
+              H.describe "baz" $ do
+                H.it "example 3" $ mockAction e3
           (,,) <$> mockCounter e1 <*> mockCounter e2 <*> mockCounter e3 `shouldReturn` (1, 1, 0)
 
         it "can be given multiple times" $ do
           e1 <- newMock
           e2 <- newMock
           e3 <- newMock
-          withArgs ["-m", "foo", "-m", "baz"] $
-            H.hspec [
-              H.describe "foo" [
-                H.it "example 1" $ mockAction e1
-              ]
-            , H.describe "bar" [
-                H.it "example 2" $ mockAction e2
-              ]
-            , H.describe "baz" [
-                H.it "example 3" $ mockAction e3
-              ]
-            ]
+          withArgs ["-m", "foo", "-m", "baz"] .  H.hspec . runSpecM $ do
+            H.describe "foo" $ do
+              H.it "example 1" $ mockAction e1
+            H.describe "bar" $ do
+              H.it "example 2" $ mockAction e2
+            H.describe "baz" $ do
+              H.it "example 3" $ mockAction e3
           (,,) <$> mockCounter e1 <*> mockCounter e2 <*> mockCounter e3 `shouldReturn` (1, 0, 1)
 
   describe "hspecWith" $ do
     it "returns a summary of the test run" $ do
-      let testSpec = [
-              H.it "foo" True
-            , H.it "foo" False
-            , H.it "foo" False
-            , H.it "foo" True
-            , H.it "foo" True
-            ]
-      H.hspecWith H.defaultConfig testSpec `shouldReturn` H.Summary 5 2
+      H.hspecWith H.defaultConfig . runSpecM $ do
+        H.it "foo" True
+        H.it "foo" False
+        H.it "foo" False
+        H.it "foo" True
+        H.it "foo" True
+      `shouldReturn` H.Summary 5 2
 
     it "uses the specdoc formatter by default" $ do
-      let testSpec = [H.describe "Foo.Bar" [H.it "some example" True]]
+      let testSpec = (runSpecM $ H.describe "Foo.Bar" $ H.it "some example" True)
       _:r:_ <- capture $ H.hspecWith H.defaultConfig testSpec
       r `shouldBe` "Foo.Bar"
 
     it "can use a custom formatter" $ do
-      let testSpec = [H.describe "Foo.Bar" []]
+      let testSpec = (runSpecM $ H.describe "Foo.Bar" $ return ())
       [] <- capture $ H.hspecWith H.defaultConfig {H.configFormatter = H.silent} testSpec
       return ()
