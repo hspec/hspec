0e0e9d866ed7e8690d25fc8c24fc6fab66df506b
Simon Hengel <sol@typeful.net>
2012-10-04 12:05:21 +0200

    Use monadic specs for Test.Hspec.FormattersSpec

--- test/Test/Hspec/FormattersSpec.hs
+++ test/Test/Hspec/FormattersSpec.hs
@@ -7,23 +7,23 @@ import           Util (capture)
 import           System.Console.ANSI
 import qualified System.IO.Silently as S
 
-import qualified Test.Hspec.Core as H
-import qualified Test.Hspec.Runner as H
+import qualified Test.Hspec.Monadic as H
+import qualified Test.Hspec.Core as H (Result(..))
+import qualified Test.Hspec.Runner as H (ColorMode(..))
 import qualified Test.Hspec.Formatters as H
 
 main :: IO ()
 main = hspec spec
 
-testSpec :: H.Specs
-testSpec = [H.describe "Example" [
+testSpec :: H.Spec
+testSpec = do
+  H.describe "Example" $ do
     H.it "success"    (H.Success)
-  , H.it "fail 1"     (H.Fail "fail message")
-  , H.it "pending"    (H.pending "pending message")
-  , H.it "fail 2"     (H.Fail "")
-  , H.it "exceptions" (undefined :: H.Result)
-  , H.it "fail 3"     (H.Fail "")
-  ]
-  ]
+    H.it "fail 1"     (H.Fail "fail message")
+    H.it "pending"    (H.pending "pending message")
+    H.it "fail 2"     (H.Fail "")
+    H.it "exceptions" (undefined :: H.Result)
+    H.it "fail 3"     (H.Fail "")
 
 spec :: Spec
 spec = do
@@ -75,7 +75,7 @@ failed_examplesSpec formatter = do
   let runSpec = capture . H.hspecWith H.defaultConfig {H.configFormatter = formatter}
 
   it "summarizes the time it takes to finish" $ do
-    r <- runSpec []
+    r <- runSpec (return ())
     r `shouldSatisfy` any ("Finished in " `isPrefixOf`)
 
   context "displays a detailed list of failures" $ do
@@ -84,14 +84,18 @@ failed_examplesSpec formatter = do
       r `shouldSatisfy` any (== "1) Example fail 1 FAILED")
 
     it "prints the exception type for requirements that fail due to an uncaught exception" $ do
-      r <- runSpec [H.it "foobar" (undefined :: Bool)]
+      r <- runSpec $ do
+        H.it "foobar" (undefined :: Bool)
       r `shouldSatisfy` isInfixOf [
           "1) foobar FAILED (uncaught exception)"
         , "GHC.Exception.ErrorCall (Prelude.undefined)"
         ]
 
     it "prints all descriptions when a nested requirement fails" $ do
-      r <- runSpec [H.describe "foo" [H.describe "bar" [H.it "baz" False]]]
+      r <- runSpec $
+        H.describe "foo" $ do
+          H.describe "bar" $ do
+            H.it "baz" False
       r `shouldSatisfy` any (== "1) foo.bar baz FAILED")
 
   it "summarizes the number of examples and failures" $ do
@@ -99,19 +103,24 @@ failed_examplesSpec formatter = do
     r `shouldSatisfy` any (== "6 examples, 1 pending, 4 failures")
 
   it "shows summary in green if there are no failures" $ do
-    r <- capture $ H.hspecWith H.defaultConfig {H.configColorMode = H.ColorAlway} [H.it "foobar" True]
+    r <- capture $ H.hspecWith H.defaultConfig {H.configColorMode = H.ColorAlway} $ do
+      H.it "foobar" True
     r `shouldSatisfy` any (== (green ++ "1 example, 0 pending, 0 failures" ++ reset))
 
   it "shows summary in yellow if there are pending examples" $ do
-    r <- capture $ H.hspecWith H.defaultConfig {H.configColorMode = H.ColorAlway} [H.it "foobar" H.pending]
+    r <- capture $ H.hspecWith H.defaultConfig {H.configColorMode = H.ColorAlway} $ do
+      H.it "foobar" H.pending
     r `shouldSatisfy` any (== (yellow ++ "1 example, 1 pending, 0 failures" ++ reset))
 
   it "shows summary in red if there are failures" $ do
-    r <- capture $ H.hspecWith H.defaultConfig {H.configColorMode = H.ColorAlway} [H.it "foobar" False]
+    r <- capture $ H.hspecWith H.defaultConfig {H.configColorMode = H.ColorAlway} $ do
+      H.it "foobar" False
     r `shouldSatisfy` any (== (red ++ "1 example, 0 pending, 1 failure" ++ reset))
 
   it "shows summary in red if there are both failures and pending examples" $ do
-    r <- capture $ H.hspecWith H.defaultConfig {H.configColorMode = H.ColorAlway} [H.it "foo" False, H.it "bar" H.pending]
+    r <- capture $ H.hspecWith H.defaultConfig {H.configColorMode = H.ColorAlway} $ do
+      H.it "foo" False
+      H.it "bar" H.pending
     r `shouldSatisfy` any (== (red ++ "2 examples, 1 pending, 1 failure" ++ reset))
   where
     green  = setSGRCode [SetColor Foreground Dull Green]
