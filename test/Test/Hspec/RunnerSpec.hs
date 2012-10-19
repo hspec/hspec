05c853cdd805245c5cfbcaa197cfa6584cc13ec3
Simon Hengel <sol@typeful.net>
2012-10-19 17:30:47 +0200

    Test suite cleanup

--- test/Test/Hspec/RunnerSpec.hs
+++ test/Test/Hspec/RunnerSpec.hs
@@ -43,125 +43,124 @@ spec = do
           putStrLn "baz"
       r `shouldSatisfy` notElem "baz"
 
-    context "command-line options" $ do
-      it "prints an error message on unrecognized options" $ do
-        withProgName "myspec" . withArgs ["--foo"] $ do
-          hSilence [stderr] (H.hspec $ pure ()) `shouldThrow` (== ExitFailure 1)
-          fst `fmap` hCapture [stderr] (ignoreExitCode (H.hspec $ pure ())) `shouldReturn` unlines [
-              "myspec: unrecognized option `--foo'"
-            , "Try `myspec --help' for more information."
-            ]
+    it "prints an error message on unrecognized command-line options" $ do
+      withProgName "myspec" . withArgs ["--foo"] $ do
+        hSilence [stderr] (H.hspec $ pure ()) `shouldThrow` (== ExitFailure 1)
+        fst `fmap` hCapture [stderr] (ignoreExitCode (H.hspec $ pure ())) `shouldReturn` unlines [
+            "myspec: unrecognized option `--foo'"
+          , "Try `myspec --help' for more information."
+          ]
 
-      it "does not leak command-line flags to examples" $ do
-        withArgs ["--verbose"] $ do
-          H.hspec $ do
-            H.it "foobar" $ do
-              getArgs `shouldReturn` []
-          `shouldReturn` ()
+    it "does not leak command-line flags to examples" $ do
+      withArgs ["--verbose"] $ do
+        H.hspec $ do
+          H.it "foobar" $ do
+            getArgs `shouldReturn` []
+        `shouldReturn` ()
 
-      describe "option '--verbose'" $ do
-        it "does not suppress output to stdout when evaluating examples" $ do
-          r <- capture__ . withArgs ["--verbose"] . H.hspec $ do
-            H.it "foobar" $ do
-              putStrLn "baz"
-          r `shouldSatisfy` elem "baz"
+    context "with --verbose" $ do
+      it "does not suppress output to stdout" $ do
+        r <- capture__ . withArgs ["--verbose"] . H.hspec $ do
+          H.it "foobar" $ do
+            putStrLn "baz"
+        r `shouldSatisfy` elem "baz"
 
-      describe "option '--match'" $ do
-        it "only runs examples that match a given pattern" $ do
-          e1 <- newMock
-          e2 <- newMock
-          e3 <- newMock
-          withArgs ["-m", "/bar/example"] . H.hspec $ do
-            H.describe "foo" $ do
-              H.describe "bar" $ do
-                H.it "example 1" $ mockAction e1
-                H.it "example 2" $ mockAction e2
-              H.describe "baz" $ do
-                H.it "example 3" $ mockAction e3
-          (,,) <$> mockCounter e1 <*> mockCounter e2 <*> mockCounter e3 `shouldReturn` (1, 1, 0)
-
-        it "can be given multiple times" $ do
-          e1 <- newMock
-          e2 <- newMock
-          e3 <- newMock
-          withArgs ["-m", "foo", "-m", "baz"] . H.hspec $ do
-            H.describe "foo" $ do
-              H.it "example 1" $ mockAction e1
+    context "with --match" $ do
+      it "only runs examples that match a given pattern" $ do
+        e1 <- newMock
+        e2 <- newMock
+        e3 <- newMock
+        withArgs ["-m", "/bar/example"] . H.hspec $ do
+          H.describe "foo" $ do
             H.describe "bar" $ do
+              H.it "example 1" $ mockAction e1
               H.it "example 2" $ mockAction e2
             H.describe "baz" $ do
               H.it "example 3" $ mockAction e3
-          (,,) <$> mockCounter e1 <*> mockCounter e2 <*> mockCounter e3 `shouldReturn` (1, 0, 1)
+        (,,) <$> mockCounter e1 <*> mockCounter e2 <*> mockCounter e3 `shouldReturn` (1, 1, 0)
 
-    describe "experimental features" $ do
-      it "keeps a failure report in the environment" $ do
-        ignoreExitCode . H.hspec $ do
+      it "can be given multiple times" $ do
+        e1 <- newMock
+        e2 <- newMock
+        e3 <- newMock
+        withArgs ["-m", "foo", "-m", "baz"] . H.hspec $ do
           H.describe "foo" $ do
-            H.describe "bar" $ do
-              H.it "example 1" True
-              H.it "example 2" False
+            H.it "example 1" $ mockAction e1
+          H.describe "bar" $ do
+            H.it "example 2" $ mockAction e2
           H.describe "baz" $ do
+            H.it "example 3" $ mockAction e3
+        (,,) <$> mockCounter e1 <*> mockCounter e2 <*> mockCounter e3 `shouldReturn` (1, 0, 1)
+
+    context "with --html" $ do
+      it "produces HTML output" $ do
+        r <- capture_ . withArgs ["--html"] . H.hspec $ do
+          H.it "foo" True
+        r `shouldStartWith` "<pre class=\"hspec-report\">"
+        r `shouldEndWith`   "</pre>\n"
+
+      it "marks successful examples with CSS class hspec-success" $ do
+        r <- capture_ . withArgs ["--html"] . H.hspec $ do
+          H.it "foo" True
+        r `shouldContain` "<span class=\"hspec-success\">- foo\n</span>"
+
+      it "marks pending examples with CSS class hspec-pending" $ do
+        r <- capture_ . withArgs ["--html"] . H.hspec $ do
+          H.it "foo" H.pending
+        r `shouldContain` "<span class=\"hspec-pending\">- foo"
+
+      it "marks failed examples with CSS class hspec-failure" $ do
+        r <- capture_ . ignoreExitCode . withArgs ["--html"] . H.hspec $ do
+          H.it "foo" False
+        r `shouldContain` "<span class=\"hspec-failure\">- foo"
+
+  describe "hspec (experimental features)" $ do
+    it "stores a failure report in environment HSPEC_FAILURES" $ do
+      ignoreExitCode . H.hspec $ do
+        H.describe "foo" $ do
+          H.describe "bar" $ do
+            H.it "example 1" True
+            H.it "example 2" False
+        H.describe "baz" $ do
+          H.it "example 3" False
+      getEnv "HSPEC_FAILURES" `shouldReturn` Just "[([\"foo\",\"bar\"],\"example 2\"),([\"baz\"],\"example 3\")]"
+
+    describe "with --re-run" $ do
+      let runSpec = (capture__ . ignoreExitCode . H.hspec) $ do
+            H.it "example 1" True
+            H.it "example 2" False
             H.it "example 3" False
-        getEnv "HSPEC_FAILURES" `shouldReturn` Just "[([\"foo\",\"bar\"],\"example 2\"),([\"baz\"],\"example 3\")]"
+            H.it "example 4" True
+            H.it "example 5" False
 
-      describe "option '--re-run'" $ do
-        let runSpec = (capture__ . ignoreExitCode . H.hspec) $ do
-              H.it "example 1" True
-              H.it "example 2" False
-              H.it "example 3" False
-              H.it "example 4" True
-              H.it "example 5" False
+      it "re-runs examples that previously failed" $ do
+        r0 <- runSpec
+        r0 `shouldSatisfy` elem "5 examples, 0 pending, 3 failures"
 
-        it "re-runs examples that previously failed" $ do
-          r0 <- runSpec
-          r0 `shouldSatisfy` elem "5 examples, 0 pending, 3 failures"
+        r1 <- withArgs ["-r"] runSpec
+        r1 `shouldSatisfy` elem "3 examples, 0 pending, 3 failures"
 
-          r1 <- withArgs ["-r"] runSpec
-          r1 `shouldSatisfy` elem "3 examples, 0 pending, 3 failures"
+      context "when there is no failure report in the environment" $ do
+        it "runs everything" $ do
+          unsetEnv "HSPEC_FAILURES"
+          r <- hSilence [stderr] $ withArgs ["-r"] runSpec
+          r `shouldSatisfy` elem "5 examples, 0 pending, 3 failures"
 
-        context "when there is no failure report in the environment" $ do
-          it "runs everything" $ do
-            unsetEnv "HSPEC_FAILURES"
-            r <- hSilence [stderr] $ withArgs ["-r"] runSpec
-            r `shouldSatisfy` elem "5 examples, 0 pending, 3 failures"
+        it "prints a warning to stderr" $ do
+          unsetEnv "HSPEC_FAILURES"
+          r <- hCapture [stderr] $ withArgs ["-r"] runSpec
+          fst r `shouldBe` "WARNING: Could not read environment variable HSPEC_FAILURES; `--re-run' is ignored!\n"
 
-          it "prints a warning to stderr" $ do
-            unsetEnv "HSPEC_FAILURES"
-            r <- hCapture [stderr] $ withArgs ["-r"] runSpec
-            fst r `shouldBe` "WARNING: Could not read environment variable HSPEC_FAILURES; `--re-run' is ignored!\n"
+      context "when parsing of failure report fails" $ do
+        it "runs everything" $ do
+          setEnv "HSPEC_FAILURES" "some invalid report"
+          r <- hSilence [stderr] $ withArgs ["-r"] runSpec
+          r `shouldSatisfy` elem "5 examples, 0 pending, 3 failures"
 
-        context "when parsing of failure report fails" $ do
-          it "runs everything" $ do
-            setEnv "HSPEC_FAILURES" "some invalid report"
-            r <- hSilence [stderr] $ withArgs ["-r"] runSpec
-            r `shouldSatisfy` elem "5 examples, 0 pending, 3 failures"
-
-          it "prints a warning to stderr" $ do
-            setEnv "HSPEC_FAILURES" "some invalid report"
-            r <- hCapture [stderr] $ withArgs ["-r"] runSpec
-            fst r `shouldBe` "WARNING: Could not read environment variable HSPEC_FAILURES; `--re-run' is ignored!\n"
-
-      context "when producing HTML (command-line option --html)" $ do
-        it "wraps output in pre-tag" $ do
-          r <- capture_ . withArgs ["--html"] . H.hspec $ do
-            H.it "foo" True
-          r `shouldStartWith` "<pre class=\"hspec-report\">"
-          r `shouldEndWith`   "</pre>\n"
-
-        it "marks successful examples with CSS class hspec-success" $ do
-          r <- capture_ . withArgs ["--html"] . H.hspec $ do
-            H.it "foo" True
-          r `shouldContain` "<span class=\"hspec-success\">- foo\n</span>"
-
-        it "marks pending examples with CSS class hspec-pending" $ do
-          r <- capture_ . withArgs ["--html"] . H.hspec $ do
-            H.it "foo" H.pending
-          r `shouldContain` "<span class=\"hspec-pending\">- foo"
-
-        it "marks failed examples with CSS class hspec-failure" $ do
-          r <- capture_ . ignoreExitCode . withArgs ["--html"] . H.hspec $ do
-            H.it "foo" False
-          r `shouldContain` "<span class=\"hspec-failure\">- foo"
+        it "prints a warning to stderr" $ do
+          setEnv "HSPEC_FAILURES" "some invalid report"
+          r <- hCapture [stderr] $ withArgs ["-r"] runSpec
+          fst r `shouldBe` "WARNING: Could not read environment variable HSPEC_FAILURES; `--re-run' is ignored!\n"
 
   describe "hspecWith" $ do
     it "returns a summary of the test run" $ do
