243f71b8af4d302aa689cc609abc545a84c57ad8
Simon Hengel <sol@typeful.net>
2012-10-20 19:34:07 +0200

    doc: Add caching for runhaskell output

--- doc/_plugins/custom.rb
+++ doc/_plugins/custom.rb
@@ -1,3 +1,4 @@
+require 'digest'
 
 # use our custom version of pygments
 Pygments.start(File.join(File.dirname(__FILE__), "../pygments"))
@@ -5,7 +6,17 @@ Pygments.start(File.join(File.dirname(__FILE__), "../pygments"))
 module Hspec
   module CustomFilters
     def runhaskell(input)
-      `runhaskell -i../src #{input}`
+      digest = Digest::MD5.hexdigest(input)
+      file   = File.join ".cache/runhaskell", digest
+      puts "#{digest} (#{input})"
+
+      if File.exists? file
+        File.read file
+      else
+        r = `runhaskell -i../src -fobject-code -outputdircache/ghc #{input}`
+        File.write file, r
+        r
+      end
     end
   end
 end
