f321c8323193c11647152eb3850cbab431161fbb
Simon Hengel <sol@typeful.net>
2012-11-04 18:01:15 +0100

    Update documentation of Test.Hspec.QuickCheck

--- src/Test/Hspec/QuickCheck.hs
+++ src/Test/Hspec/QuickCheck.hs
@@ -1,29 +1,20 @@
--- |
--- Importing this module allows you to use a QuickCheck `QC.Property` as an
--- example for a behavior.  Use `QC.property` to turn any `QC.Testable` into a
--- @Property@.
---
--- NOTE: Any output from the example to @stdout@ is ignored.  If you need to
--- write out for debugging, you can write to @stderr@ or a file handle.
---
--- > import Test.Hspec
--- > import Test.QuickCheck
--- >
--- > main :: IO ()
--- > main = hspec $ do
--- >   describe "reverse" $ do
--- >     it "gives the original list, if applied twice" $ property $
--- >       \xs -> (reverse . reverse) xs == (xs :: [Int])
 module Test.Hspec.QuickCheck (
-  QC.property
+-- * Re-exports from QuickCheck
+  property
+-- * Shortcuts
 , prop
 ) where
 
-import qualified Test.QuickCheck as QC
+import           Test.QuickCheck
+import           Test.Hspec.Monadic
 
--- just for the prop shortcut
-import qualified Test.Hspec.Monadic as DSL
-
--- | Monadic DSL shortcut, use this instead of `DSL.it`.
-prop :: QC.Testable t => String -> t -> DSL.Spec
-prop n p = DSL.it n (QC.property p)
+-- |
+-- > prop ".." $
+-- >   ..
+--
+-- is a shortcut for
+--
+-- > it ".." $ property $
+-- >   ..
+prop :: Testable prop => String -> prop -> Spec
+prop s = it s . property
