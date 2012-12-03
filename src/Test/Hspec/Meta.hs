19b6073b06fdae7bc6e7b38588056d9c72af9360
Simon Hengel <sol@typeful.net>
2012-12-03 09:13:13 +0100

    Rename package to hspec-meta

---
+++ src/Test/Hspec/Meta.hs
@@ -0,0 +1,5 @@
+{-# OPTIONS_GHC -fno-warn-deprecations #-}
+module Test.Hspec.Meta (module Test.Hspec, module Test.Hspec.Discover) where
+
+import           Test.Hspec
+import           Test.Hspec.Discover
