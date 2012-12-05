8ce85f0fe3686ebd603985454ceec1003f00624b
Simon Hengel <sol@typeful.net>
2012-12-05 10:16:48 +0100

    Make sure that full exception type is printed with base-4.4.*

--- src/Test/Hspec/Compat.hs
+++ src/Test/Hspec/Compat.hs
@@ -3,14 +3,14 @@ module Test.Hspec.Compat where
 
 import           Data.Typeable (Typeable, typeOf, typeRepTyCon)
 
-#if MIN_VERSION_base(4,5,0)
-import           Data.Typeable (tyConModule, tyConName)
+#if MIN_VERSION_base(4,4,0)
+import           Data.Typeable.Internal (tyConModule, tyConName)
 #endif
 
 
 showType :: Typeable a => a -> String
 showType a = let t = typeRepTyCon (typeOf a) in
-#if MIN_VERSION_base(4,5,0)
+#if MIN_VERSION_base(4,4,0)
   tyConModule t ++ "." ++ tyConName t
 #else
   show t
