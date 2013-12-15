1de1c9e94a07505fb006bf5cb0ee88d157e38b4a
Simon Hengel <sol@typeful.net>
2013-12-15 21:51:08 +0100

    Move integration test for hspec-discover to a separate Cabal package

--- mk-sdist.sh
+++ mk-sdist.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
+
 set -o errexit
 set -o nounset
 
 cabal clean
-
-# disable tests that require hspec-discover on the path
-sed -i 's/^test-suite hspec-discover-example$/\0\n  buildable: False/' hspec.cabal
-sed -i 's/^test-suite hspec-discover-integration-test-empty$/\0\n  buildable: False/' hspec.cabal
-sed -i 's/^test-suite hspec-discover-integration-test-with-formatter$/\0\n  buildable: False/' hspec.cabal
-sed -i 's/^test-suite hspec-discover-integration-test-with-io-formatter$/\0\n  buildable: False/' hspec.cabal
-sed -i 's/^test-suite hspec-discover-integration-test-with-formatter-empty$/\0\n  buildable: False/' hspec.cabal
-
 cabal sdist
-
-# enable again
-sed -i '/^  buildable: False$/d' hspec.cabal
 
 cd dist/
 tar xvf hspec-1.*.tar.gz
