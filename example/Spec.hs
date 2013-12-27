45aaf37b8ebad35ef14cc6e4b1d35e221b6b754b
Art <cipherwraith666@gmail.com>
2013-12-27 21:35:51 +0800

    Moved a few examples into the example Spec.hs file
    Took these examples out of the /master/src/Test/Hspec/Core/Type.hs source code, then reformatted them for this new example.

--- example/Spec.hs
+++ example/Spec.hs
@@ -8,9 +8,30 @@ main = hspec spec
 
 spec :: Spec
 spec = do
+
+  -- | Specify a concrete example.
+  --
+  -- Describe a "reverse" function, providing two examples
+  -- to test its functionality:
+  
   describe "reverse" $ do
     it "reverses a list" $ do
       reverse [1 :: Int, 2, 3] `shouldBe` [3, 2, 1]
 
     it "gives the original list, if applied twice" $ property $
       \xs -> (reverse . reverse) xs == (xs :: [Int])
+
+  -- | Specify a pending example.
+  --
+  -- If you want to textually specify a behavior but do not have an example yet,
+  -- use this:
+
+  describe "fancyFormatter" $ do
+    it "can format text in a way that everyone lines" $
+      pending
+
+  -- | Specify a pending example with a reason for why it's pending.
+
+  describe "fancyFormatter" $ do
+    it "can format text in a way that everyone likes" $
+      pendingWith "waiting for clarification from the designers"
