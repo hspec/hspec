8e13c8fa1616b5729799cfa367d7ec6273a58976
Daniel Gröber <dxld@darkboxed.org>
2014-01-26 23:49:31 +0100

    Add --no-main option to hspec-discover

--- hspec-discover/src/Config.hs
+++ hspec-discover/src/Config.hs
@@ -5,28 +5,38 @@ module Config (
 , usage
 ) where
 
+import           Data.Maybe
 import           System.Console.GetOpt
 
 data Config = Config {
   configNested :: Bool
 , configFormatter :: Maybe String
+, configNoMain :: Bool
 } deriving (Eq, Show)
 
 defaultConfig :: Config
-defaultConfig = Config False Nothing
+defaultConfig = Config False Nothing False
 
 options :: [OptDescr (Config -> Config)]
 options = [
     Option [] ["nested"]    (NoArg  $ \c -> c   {configNested = True}) ""
   , Option [] ["formatter"] (ReqArg (\s c -> c {configFormatter = Just s}) "FORMATTER") ""
+  , Option [] ["no-main"]   (NoArg  $ \c -> c   {configNoMain = True}) ""
   ]
 
 usage :: String -> String
-usage prog = "\nUsage: " ++ prog ++ " SRC CUR DST [--formatter=FORMATTER]\n"
+usage prog = "\nUsage: " ++ prog ++ " SRC CUR DST [--formatter=FORMATTER] [--no-main]\n"
 
 parseConfig :: String -> [String] -> Either String Config
 parseConfig prog args = case getOpt Permute options args of
-    (opts, [], []) -> Right (foldl (flip id) defaultConfig opts)
+    (opts, [], []) -> let
+        c = (foldl (flip id) defaultConfig opts)
+      in
+        if (configNoMain c && isJust (configFormatter c))
+           then
+             formatError "option `--formatter=<fmt>' does not make sense with `--no-main'\n"
+           else
+             Right c
     (_, _, err:_)  -> formatError err
     (_, arg:_, _)  -> formatError ("unexpected argument `" ++ arg ++ "'\n")
   where
--- hspec-discover/test/ConfigSpec.hs
+++ hspec-discover/test/ConfigSpec.hs
@@ -18,18 +18,28 @@ spec = do
     it "recognizes --formatter" $ do
       parse ["--formatter", "someFormatter"] `shouldBe` Right (defaultConfig {configFormatter = Just "someFormatter"})
 
+    it "recognizes --no-main" $ do
+      parse ["--no-main"] `shouldBe` Right (defaultConfig {configNoMain = True})
+
     it "returns error message on unrecognized option" $ do
       parse ["--foo"] `shouldBe` (Left . unlines) [
           "hspec-discover: unrecognized option `--foo'"
         , ""
-        , "Usage: hspec-discover SRC CUR DST [--formatter=FORMATTER]"
+        , "Usage: hspec-discover SRC CUR DST [--formatter=FORMATTER] [--no-main]"
         ]
 
     it "returns error message on unexpected argument" $ do
       parse ["foo"]   `shouldBe` (Left . unlines) [
           "hspec-discover: unexpected argument `foo'"
         , ""
-        , "Usage: hspec-discover SRC CUR DST [--formatter=FORMATTER]"
+        , "Usage: hspec-discover SRC CUR DST [--formatter=FORMATTER] [--no-main]"
+        ]
+
+    it "returns error message on --formatter=<fmt> with --no-main" $ do
+      parse ["--no-main", "--formatter=foo"] `shouldBe` (Left . unlines) [
+          "hspec-discover: option `--formatter=<fmt>' does not make sense with `--no-main'"
+        , ""
+        , "Usage: hspec-discover SRC CUR DST [--formatter=FORMATTER] [--no-main]"
         ]
 
     context "when option is given multiple times" $ do
