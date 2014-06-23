d9b76328f35e225ad7f28b07219ead7107e920dd
Simon Hengel <sol@typeful.net>
2014-06-23 10:58:54 +0800

    hspec-example: Update source code comment

--- hspec-example/test/Data/String/StripSpec.hs
+++ hspec-example/test/Data/String/StripSpec.hs
@@ -3,8 +3,8 @@ module Data.String.StripSpec (main, spec) where
 import Test.Hspec
 import Data.String.Strip
 
--- This is here so that this module can be run from GHCi on its own.  It is not
--- needed for automatic spec discovery.
+-- `main` is here so that this module can be run from GHCi on its own.  It is
+-- not needed for automatic spec discovery.
 main :: IO ()
 main = hspec spec
 
