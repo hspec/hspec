1931e52d9fa502456156099a30baf6f11e1ad44e
Junji Hashimoto <junji.hashimoto@gree.net>
2014-10-21 12:39:30 +0900

    Add hspec-contrib with retryWith-function

---
+++ hspec-contrib/src/Test/Hspec/Contrib.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE CPP, TypeSynonymInstances, FlexibleInstances, GeneralizedNewtypeDeriving, DeriveDataTypeable #-}
+module Test.Hspec.Contrib (
+  retryWith
+) where
+
+import           Test.Hspec.Core
+
+data Retry a = Retry Int a
+
+instance Example a => Example (Retry a) where
+  evaluateExample (Retry n example) a b c = do
+    v <- evaluateExample example a b c
+    case v of
+      Success -> return v
+      _ | n > 1 -> evaluateExample (Retry (pred n) example) a b c
+      _ -> return v
+
+-- | Retry evaluating example that may be failed until success.
+retryWith :: Int
+          -- ^ number of retries, when this number is 1, just evaluate example and finish.
+          -> a
+          -- ^ retried example
+          -> Retry a
+          -- ^ Retry is instance of Example.
+retryWith = Retry
