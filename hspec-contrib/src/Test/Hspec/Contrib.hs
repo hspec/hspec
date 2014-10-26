5f0e8355b295dbbf5e316e1462bdd5fe8d0c47ee
Simon Hengel <sol@typeful.net>
2014-10-26 23:57:44 +0800

    hspec-contrib: Adapt for argument support

--- hspec-contrib/src/Test/Hspec/Contrib.hs
+++ hspec-contrib/src/Test/Hspec/Contrib.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeFamilies #-}
 module Test.Hspec.Contrib (
   retryWith
 ) where
@@ -7,6 +8,7 @@ import           Test.Hspec.Core
 data Retry a = Retry Int a
 
 instance Example a => Example (Retry a) where
+  type Arg (Retry a) = Arg a
   evaluateExample (Retry n example) a b c = do
     v <- evaluateExample example a b c
     case v of
