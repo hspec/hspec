ee2845012310bc38e7cebc46241ef251a3315a8b
Simon Hengel <sol@typeful.net>
2014-11-01 16:52:27 +0800

    GHC 7.0.1 compatibility

--- src/Test/Hspec/Runner/Tree.hs
+++ src/Test/Hspec/Runner/Tree.hs
@@ -1,16 +1,35 @@
-{-# LANGUAGE DeriveFunctor, DeriveFoldable, DeriveTraversable #-}
+{-# LANGUAGE CPP, DeriveFunctor #-}
 module Test.Hspec.Runner.Tree where
 
 import           Control.Applicative
-import           Data.Foldable (Foldable)
-import           Data.Traversable (Traversable)
+import           Data.Foldable
+import           Data.Traversable
 import           Test.Hspec.Core.Type
+import           Data.Monoid
 
 data Tree a
   = Node !String [Tree a]
   | NodeWithCleanup (IO ()) [Tree a]
   | Leaf a
-  deriving (Functor, Foldable, Traversable)
+  deriving Functor
+
+instance Foldable Tree where -- Note: GHC 7.0.1 fails to derive this instance
+  foldMap = go
+    where
+      go :: Monoid m => (a -> m) -> Tree a -> m
+      go f t = case t of
+        Node _ xs -> foldMap (foldMap f) xs
+        NodeWithCleanup _ xs -> foldMap (foldMap f) xs
+        Leaf x -> f x
+
+instance Traversable Tree where -- Note: GHC 7.0.1 fails to derive this instance
+  sequenceA = go
+    where
+      go :: Applicative f => Tree (f a) -> f (Tree a)
+      go t = case t of
+        Node label xs -> Node label <$> sequenceA (map go xs)
+        NodeWithCleanup action xs -> NodeWithCleanup action <$> sequenceA (map go xs)
+        Leaf a -> Leaf <$> a
 
 toTree :: Spec -> IO [Tree Item]
 toTree spec = map f <$> runSpecM spec
