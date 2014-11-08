775fc688837049380bdf4b5d6e813c7df4811967
Simon Hengel <sol@typeful.net>
2014-11-09 00:13:59 +0800

    Generalize type of modifyMaxSuccess, etc.

--- hspec-core/src/Test/Hspec/Core/QuickCheck.hs
+++ hspec-core/src/Test/Hspec/Core/QuickCheck.hs
@@ -13,27 +13,27 @@ import           Test.Hspec.Core.Spec
 import           Test.Hspec.Core (Params(..), modifyParams)
 
 -- | Use a modified `maxSuccess` for given spec.
-modifyMaxSuccess :: (Int -> Int) -> Spec -> Spec
+modifyMaxSuccess :: (Int -> Int) -> SpecWith a -> SpecWith a
 modifyMaxSuccess = modifyArgs . modify
   where
     modify :: (Int -> Int) -> Args -> Args
     modify f args = args {maxSuccess = f (maxSuccess args)}
 
 -- | Use a modified `maxDiscardRatio` for given spec.
-modifyMaxDiscardRatio :: (Int -> Int) -> Spec -> Spec
+modifyMaxDiscardRatio :: (Int -> Int) -> SpecWith a -> SpecWith a
 modifyMaxDiscardRatio = modifyArgs . modify
   where
     modify :: (Int -> Int) -> Args -> Args
     modify f args = args {maxDiscardRatio = f (maxDiscardRatio args)}
 
 -- | Use a modified `maxSize` for given spec.
-modifyMaxSize :: (Int -> Int) -> Spec -> Spec
+modifyMaxSize :: (Int -> Int) -> SpecWith a -> SpecWith a
 modifyMaxSize = modifyArgs . modify
   where
     modify :: (Int -> Int) -> Args -> Args
     modify f args = args {maxSize = f (maxSize args)}
 
-modifyArgs :: (Args -> Args) -> Spec -> Spec
+modifyArgs :: (Args -> Args) -> SpecWith a -> SpecWith a
 modifyArgs = modifyParams . modify
   where
     modify :: (Args -> Args) -> Params -> Params
