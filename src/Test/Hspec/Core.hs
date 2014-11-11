9a1bd27ecc460e369e2ddaf14860d796c849f766
Simon Hengel <sol@typeful.net>
2014-11-11 19:13:10 +0800

    Re-export hspec-2.0

--- src/Test/Hspec/Core.hs
+++ src/Test/Hspec/Core.hs
@@ -1,37 +1,4 @@
--- |
--- Stability: experimental
---
--- This module provides access to Hspec's internals.  It is less stable than
--- other parts of the API.  For most users "Test.Hspec" is more suitable!
-module Test.Hspec.Core (
-
--- * A type class for examples
-  Example (..)
-, Params (..)
-, Progress
-, ProgressCallback
-, Result (..)
-
--- * A writer monad for constructing specs
-, SpecM
-, runSpecM
-, fromSpecList
-
--- * Internal representation of a spec tree
-, SpecTree (..)
-, mapSpecTree
-, Item (..)
-, Location (..)
-, LocationAccuracy(..)
-, ActionWith
-, mapSpecItem
-, mapSpecItem_
-, modifyParams
-, describe
-, it
-) where
-
-import           Test.Hspec.Core.Type
-
-modifyParams :: (Params -> Params) -> SpecWith a -> SpecWith a
-modifyParams f = mapSpecItem_ $ \item -> item {itemExample = \p -> (itemExample item) (f p)}
+{-# LANGUAGE PackageImports #-}
+{-# OPTIONS_GHC -fno-warn-deprecations #-}
+module Test.Hspec.Core (module Test.Hspec.Core) where
+import "hspec" Test.Hspec.Core
