b8f60fdf22f500b5d781a6210df9e0b18aacfa56
Simon Hengel <sol@typeful.net>
2014-11-19 11:51:22 +0800

    hspec-contrib: Add specListFromHUnitTest to Test.Hspec.Contrib.HUnit

--- hspec-contrib/src/Test/Hspec/Contrib/HUnit.hs
+++ hspec-contrib/src/Test/Hspec/Contrib/HUnit.hs
@@ -3,6 +3,7 @@
 module Test.Hspec.Contrib.HUnit (
 -- * Interoperability with HUnit
   fromHUnitTest
+, specListFromHUnitTest
 ) where
 
 import           Test.Hspec.Core.Spec
@@ -12,14 +13,20 @@ import           Test.HUnit (Test (..))
 -- Convert a HUnit test suite to a spec.  This can be used to run existing
 -- HUnit tests with Hspec.
 fromHUnitTest :: Test -> Spec
-fromHUnitTest t = case t of
-  TestList xs -> mapM_ go xs
-  x -> go x
+fromHUnitTest = fromSpecList . specListFromHUnitTest
+
+-- |
+-- @specListFromHUnitTest@ is similar to `fromHUnitTest`, but it constructs a
+-- list of `SpecTree`s instead of a `Spec`.
+specListFromHUnitTest :: Test -> [SpecTree ()]
+specListFromHUnitTest t = case t of
+  TestList xs -> map go xs
+  x -> [go x]
   where
-    go :: Test -> Spec
+    go :: Test -> SpecTree ()
     go t_ = case t_ of
-      TestLabel s (TestCase e) -> it s e
-      TestLabel s (TestList xs) -> describe s (mapM_ go xs)
-      TestLabel s x -> describe s (go x)
-      TestList xs -> describe "<unlabeled>" (mapM_ go xs)
-      TestCase e -> it "<unlabeled>" e
+      TestLabel s (TestCase e) -> specItem s e
+      TestLabel s (TestList xs) -> specGroup s (map go xs)
+      TestLabel s x -> specGroup s [go x]
+      TestList xs -> specGroup "<unlabeled>" (map go xs)
+      TestCase e -> specItem "<unlabeled>" e
