3bf50f54e51aa9d2f606098d0bf73582df2dac4a
Julian K. Arni <jkarni@gmail.com>
2015-04-10 11:05:37 +0200

    Add captureLogs function

---
+++ hspec-contrib/src/Test/Hspec/Contrib/LoggingFacade.hs
@@ -0,0 +1,25 @@
+module Test.Hspec.Contrib.LoggingFacade (
+  captureLogs
+) where
+
+import           Control.Exception
+import           Data.IORef
+import           System.Logging.Facade.Types
+import           System.Logging.Facade.Sink
+
+-- | Capture all logs produced by an IO action.
+-- Logs are kept in memory.
+captureLogs :: IO a -> IO ([LogRecord], a)
+captureLogs action = bracket enter exit act
+  where
+    logToRef ref record = atomicModifyIORef' ref $ \logs -> (record : logs, ())
+    enter = do
+      oldSink <- getLogSink
+      ref <- newIORef []
+      setLogSink $ logToRef ref
+      return (oldSink, ref)
+    exit (oldSink, _) = setLogSink oldSink
+    act (_, ref)  = do
+      val <- action
+      logs <- readIORef ref
+      return (reverse logs, val)
---
+++ hspec-contrib/test/Test/Hspec/Contrib/LoggingFacadeSpec.hs
@@ -0,0 +1,32 @@
+module Test.Hspec.Contrib.LoggingFacadeSpec (main, spec) where
+
+import Test.Hspec
+import Data.IORef
+import System.Logging.Facade.Types
+import System.Logging.Facade.Sink
+import System.Logging.Facade as Log
+
+import Test.Hspec.Contrib.LoggingFacade
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = describe "Hspec.Contrib.LoggingFacade" $ do
+  describe "captureLogs" $ do
+    let logToIORef :: IORef [LogRecord] -> LogSink
+        logToIORef ref record = modifyIORef ref (record :)
+
+    it "returns all logs of an action" $ do
+      (logs, ()) <- captureLogs $ do
+        Log.trace "this should be captured"
+        Log.trace "this should be captured next"
+      logs `shouldBe` [ LogRecord TRACE Nothing "this should be captured"
+                      , LogRecord TRACE Nothing "this should be captured next"
+                      ]
+
+    it "prevents logs from going to the log sink" $ do
+      ref <- newIORef []
+      setLogSink $ logToIORef ref
+      _ <- captureLogs $ Log.trace "this should be captured"
+      readIORef ref `shouldReturn` []
