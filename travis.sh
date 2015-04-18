0bec2cd9039cc5c849a503f2887b39dec05eaadd
Simon Hengel <sol@typeful.net>
2015-04-18 12:40:18 +0800

    Fix build (again!)

--- travis.sh
+++ travis.sh
@@ -9,7 +9,7 @@ rm -rf hspec-example && git clone https://github.com/hspec/hspec-example
 (                     cabal install --only-dependencies --enable-tests && cabal configure --enable-tests --ghc-options=-Werror && cabal build && cabal test && cabal sdist && cabal install dist/hspec-*.tar.gz)
 (cd hspec-example                                                      && cabal configure --enable-tests --ghc-options=-Werror && cabal build && cabal test)
 (cd hspec-th                                                           && cabal configure --enable-tests --ghc-options=-Werror && cabal build && cabal test && cabal sdist && cabal install dist/hspec-*.tar.gz)
-(cd hspec-contrib     cabal install --only-dependencies --enable-tests && cabal configure --enable-tests --ghc-options=-Werror && cabal build && cabal test && cabal sdist && cabal install dist/hspec-*.tar.gz)
+(cd hspec-contrib  && cabal install --only-dependencies --enable-tests && cabal configure --enable-tests --ghc-options=-Werror && cabal build && cabal test && cabal sdist && cabal install dist/hspec-*.tar.gz)
 (cd hspec-discover/example                                             && cabal configure --enable-tests --ghc-options=-Werror && cabal build && cabal test)
 (cd hspec-discover/integration-test                                    && cabal configure --enable-tests --ghc-options=-Werror && cabal build && cabal test)
 test/regression/issue-169/run.sh
