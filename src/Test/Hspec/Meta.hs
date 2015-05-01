ec9deb32e6dd0aa1778b195978efaa37d8aa25f3
Simon Hengel <sol@typeful.net>
2015-05-01 22:01:27 +0800

    Export hspecResult

--- src/Test/Hspec/Meta.hs
+++ src/Test/Hspec/Meta.hs
@@ -1,5 +1,10 @@
 {-# OPTIONS_GHC -fno-warn-deprecations #-}
-module Test.Hspec.Meta (module Test.Hspec, module Test.Hspec.Discover) where
+module Test.Hspec.Meta (
+  module Test.Hspec
+, module Test.Hspec.Discover
+, hspecResult
+) where
 
 import           Test.Hspec
+import           Test.Hspec.Runner
 import           Test.Hspec.Discover
