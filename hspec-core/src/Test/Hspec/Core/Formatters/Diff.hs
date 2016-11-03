b62a51a498f8a25af05e00bab61b5124db2b0c01
Simon Hengel <sol@typeful.net>
2016-11-03 12:32:22 +0800

    Improve --diff

--- hspec-core/src/Test/Hspec/Core/Formatters/Diff.hs
+++ hspec-core/src/Test/Hspec/Core/Formatters/Diff.hs
@@ -3,6 +3,7 @@ module Test.Hspec.Core.Formatters.Diff (
   Diff (..)
 , diff
 #ifdef TEST
+, partition
 , breakList
 #endif
 ) where
@@ -10,15 +11,23 @@ module Test.Hspec.Core.Formatters.Diff (
 import           Data.Char
 import           Data.Algorithm.Diff
 
+diff :: String -> String -> [Diff String]
+diff expected actual = map (fmap concat) $ getGroupedDiff (partition expected) (partition actual)
+
+partition :: String -> [String]
+partition = mergeBackslashes . breakList isAlphaNum
+  where
+    mergeBackslashes xs = case xs of
+      ['\\'] : (y : ys) : zs -> ['\\', y] : ys : mergeBackslashes zs
+      z : zs -> z : mergeBackslashes zs
+      [] -> []
+
 breakList :: (a -> Bool) -> [a] -> [[a]]
 breakList _ [] = []
 breakList p xs = case break p xs of
-  (y, ys) -> y `cons` case span p ys of
+  (y, ys) -> map return y ++ case span p ys of
     (z, zs) -> z `cons` breakList p zs
   where
     cons x
       | null x = id
       | otherwise = (x :)
-
-diff :: String -> String -> [Diff String]
-diff expected actual = map (fmap concat) $ getGroupedDiff (breakList isAlphaNum expected) (breakList isAlphaNum actual)
--- hspec-core/test/Test/Hspec/Core/Formatters/DiffSpec.hs
+++ hspec-core/test/Test/Hspec/Core/Formatters/DiffSpec.hs
@@ -7,11 +7,15 @@ import           Test.Hspec.Core.Formatters.Diff
 
 spec :: Spec
 spec = do
+  describe "partition" $ do
+    it "puts backslash-escaped characters into a separate chunks" $ do
+      partition (show "foo\nbar") `shouldBe` ["\"", "foo", "\\n", "bar", "\""]
+
   describe "breakList" $ do
     context "with a list where the predicate matches at the beginning and the end" $ do
       it "breaks the list into pieces" $ do
-        breakList isAlphaNum "foo bar  baz" `shouldBe` ["foo", " ", "bar", "  ", "baz"]
+        breakList isAlphaNum "foo bar  baz" `shouldBe` ["foo", " ", "bar", " ", " ", "baz"]
 
     context "with a list where the predicate does not match at the beginning and the end" $ do
       it "breaks the list into pieces" $ do
-        breakList isAlphaNum "  foo bar  baz  " `shouldBe` ["  ", "foo", " ", "bar", "  ", "baz", "  "]
+        breakList isAlphaNum "  foo bar  baz  " `shouldBe` [" ", " ", "foo", " ", "bar", " ", " ", "baz", " ", " "]
