5e72a3ce2cd2ff1f1a50c04f9e6facdde6f84309
Simon Hengel <sol@typeful.net>
2017-07-03 12:40:49 +0800

    Minor refactoring

--- hspec-core/test/Test/Hspec/Core/ExampleSpec.hs
+++ hspec-core/test/Test/Hspec/Core/ExampleSpec.hs
@@ -1,4 +1,6 @@
-{-# LANGUAGE CPP, ScopedTypeVariables, TypeFamilies #-}
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
 module Test.Hspec.Core.ExampleSpec (spec) where
 
 import           Helper
@@ -6,20 +8,21 @@ import           Mock
 import           Control.Exception
 import           Test.HUnit
 
+import           Test.Hspec.Core.Example (Result(..), Location(..), FailureReason(..))
 import qualified Test.Hspec.Core.Example as H
 import qualified Test.Hspec.Core.Spec as H
 import qualified Test.Hspec.Core.Runner as H
 
-safeEvaluateExample :: (H.Example e,  H.Arg e ~ ()) => e -> IO (Either SomeException H.Result)
+safeEvaluateExample :: (H.Example e,  H.Arg e ~ ()) => e -> IO (Either SomeException Result)
 safeEvaluateExample e = H.safeEvaluateExample e defaultParams ($ ()) noOpProgressCallback
 
-evaluateExample :: (H.Example e,  H.Arg e ~ ()) => e -> IO H.Result
+evaluateExample :: (H.Example e,  H.Arg e ~ ()) => e -> IO Result
 evaluateExample e = H.evaluateExample e defaultParams ($ ()) noOpProgressCallback
 
-evaluateExampleWith :: (H.Example e, H.Arg e ~ ()) => (IO () -> IO ()) -> e -> IO H.Result
+evaluateExampleWith :: (H.Example e, H.Arg e ~ ()) => (IO () -> IO ()) -> e -> IO Result
 evaluateExampleWith action e = H.evaluateExample e defaultParams (action . ($ ())) noOpProgressCallback
 
-evaluateExampleWithArgument :: H.Example e => (ActionWith (H.Arg e) -> IO ()) -> e -> IO H.Result
+evaluateExampleWithArgument :: H.Example e => (ActionWith (H.Arg e) -> IO ()) -> e -> IO Result
 evaluateExampleWithArgument action e = H.evaluateExample e defaultParams action noOpProgressCallback
 
 spec :: Spec
@@ -27,11 +30,11 @@ spec = do
   describe "safeEvaluateExample" $ do
     context "for Expectation" $ do
       it "returns Failure if an expectation does not hold" $ do
-        Right (H.Failure _ msg) <- safeEvaluateExample (23 `shouldBe` (42 :: Int))
+        Right (Failure _ msg) <- safeEvaluateExample (23 `shouldBe` (42 :: Int))
 #if MIN_VERSION_HUnit(1,5,0)
-        msg `shouldBe` H.ExpectedButGot Nothing "42" "23"
+        msg `shouldBe` ExpectedButGot Nothing "42" "23"
 #else
-        msg `shouldBe` H.Reason "expected: 42\n but got: 23"
+        msg `shouldBe` Reason "expected: 42\n but got: 23"
 #endif
 
       context "when used with `pending`" $ do
@@ -39,22 +42,22 @@ spec = do
           Right result <- safeEvaluateExample (H.pending)
           let location =
 #if MIN_VERSION_base(4,8,1)
-                Just $ H.Location __FILE__ (__LINE__ - 3) 48
+                Just $ Location __FILE__ (__LINE__ - 3) 48
 #else
                 Nothing
 #endif
-          result `shouldBe` H.Pending location Nothing
+          result `shouldBe` Pending location Nothing
 
       context "when used with `pendingWith`" $ do
         it "includes the optional reason" $ do
           Right result <- safeEvaluateExample (H.pendingWith "foo")
           let location =
 #if MIN_VERSION_base(4,8,1)
-                Just $ H.Location __FILE__ (__LINE__ - 3) 48
+                Just $ Location __FILE__ (__LINE__ - 3) 48
 #else
                 Nothing
 #endif
-          result `shouldBe` H.Pending location (Just "foo")
+          result `shouldBe` Pending location (Just "foo")
 
   describe "evaluateExample" $ do
     context "for Result" $ do
@@ -64,7 +67,7 @@ spec = do
             action e = do
               e
               modifyIORef ref succ
-        evaluateExampleWith action (H.Failure Nothing H.NoReason) `shouldReturn` H.Failure Nothing H.NoReason
+        evaluateExampleWith action (Failure Nothing NoReason) `shouldReturn` Failure Nothing NoReason
         readIORef ref `shouldReturn` 1
 
       it "accepts arguments" $ do
@@ -73,15 +76,15 @@ spec = do
             action e = do
               e 42
               modifyIORef ref succ
-        evaluateExampleWithArgument action (H.Failure Nothing . H.Reason . show) `shouldReturn` H.Failure Nothing (H.Reason "42")
+        evaluateExampleWithArgument action (Failure Nothing . Reason . show) `shouldReturn` Failure Nothing (Reason "42")
         readIORef ref `shouldReturn` 1
 
     context "for Bool" $ do
       it "returns Success on True" $ do
-        evaluateExample True `shouldReturn` (H.Success "")
+        evaluateExample True `shouldReturn` (Success "")
 
       it "returns Failure on False" $ do
-        evaluateExample False `shouldReturn` H.Failure Nothing H.NoReason
+        evaluateExample False `shouldReturn` Failure Nothing NoReason
 
       it "propagates exceptions" $ do
         evaluateExample (error "foobar" :: Bool) `shouldThrow` errorCall "foobar"
@@ -92,7 +95,7 @@ spec = do
             action e = do
               e
               modifyIORef ref succ
-        evaluateExampleWith action False `shouldReturn` H.Failure Nothing H.NoReason
+        evaluateExampleWith action False `shouldReturn` Failure Nothing NoReason
         readIORef ref `shouldReturn` 1
 
       it "accepts arguments" $ do
@@ -101,12 +104,12 @@ spec = do
             action e = do
               e 42
               modifyIORef ref succ
-        evaluateExampleWithArgument action (== (23 :: Integer)) `shouldReturn` H.Failure Nothing H.NoReason
+        evaluateExampleWithArgument action (== (23 :: Integer)) `shouldReturn` Failure Nothing NoReason
         readIORef ref `shouldReturn` 1
 
     context "for Expectation" $ do
       it "returns Success if all expectations hold" $ do
-        evaluateExample (23 `shouldBe` (23 :: Int)) `shouldReturn` (H.Success "")
+        evaluateExample (23 `shouldBe` (23 :: Int)) `shouldReturn` (Success "")
 
       it "propagates exceptions" $ do
         evaluateExample (error "foobar" :: Expectation) `shouldThrow` errorCall "foobar"
@@ -119,15 +122,15 @@ spec = do
               e
               readIORef ref `shouldReturn` succ n
               modifyIORef ref succ
-        evaluateExampleWith action (modifyIORef ref succ) `shouldReturn` (H.Success "")
+        evaluateExampleWith action (modifyIORef ref succ) `shouldReturn` (Success "")
         readIORef ref `shouldReturn` 2
 
     context "for Property" $ do
       it "returns Success if property holds" $ do
-        evaluateExample (property $ \n -> n == (n :: Int)) `shouldReturn` (H.Success "")
+        evaluateExample (property $ \n -> n == (n :: Int)) `shouldReturn` (Success "")
 
       it "shows the collected labels" $ do
-        H.Success details <- evaluateExample $ property $ \ () -> label "unit" True
+        Success details <- evaluateExample $ property $ \ () -> label "unit" True
 #if MIN_VERSION_QuickCheck(2,10,0)
         details `shouldBe` "100.0% unit\n"
 #else
@@ -135,12 +138,12 @@ spec = do
 #endif
 
       it "returns Failure if property does not hold" $ do
-        H.Failure _ _ <- evaluateExample $ property $ \n -> n /= (n :: Int)
+        Failure _ _ <- evaluateExample $ property $ \n -> n /= (n :: Int)
         return ()
 
       it "shows what falsified it" $ do
-        H.Failure _ r <- evaluateExample $ property $ \ (x :: Int) (y :: Int) -> (x == 0 && y == 1) ==> False
-        r `shouldBe` (H.Reason . intercalate "\n")  [
+        Failure _ r <- evaluateExample $ property $ \ (x :: Int) (y :: Int) -> (x == 0 && y == 1) ==> False
+        r `shouldBe` (Reason . intercalate "\n")  [
             "Falsifiable (after 1 test):"
           , "  0"
           , "  1"
@@ -154,12 +157,12 @@ spec = do
               e
               readIORef ref `shouldReturn` succ n
               modifyIORef ref succ
-        H.Success "" <- evaluateExampleWith action (property $ \(_ :: Int) -> modifyIORef ref succ)
+        Success "" <- evaluateExampleWith action (property $ \(_ :: Int) -> modifyIORef ref succ)
         readIORef ref `shouldReturn` 2000
 
       it "pretty-prints exceptions" $ do
-        H.Failure _ r <- evaluateExample $ property (\ (x :: Int) -> (x == 0) ==> (throw (ErrorCall "foobar") :: Bool))
-        r `shouldBe` (H.Reason . intercalate "\n") [
+        Failure _ r <- evaluateExample $ property (\ (x :: Int) -> (x == 0) ==> (throw (ErrorCall "foobar") :: Bool))
+        r `shouldBe` (Reason . intercalate "\n") [
             "uncaught exception: ErrorCall"
           , "foobar"
           , "(after 1 test)"
@@ -170,46 +173,46 @@ spec = do
         let prop p = property $ \ (x :: Int) (y :: Int) -> (x == 0 && y == 1) ==> p
         context "when used with shouldBe" $ do
           it "shows what falsified it" $ do
-            H.Failure _ err <- evaluateExample $ prop $ 23 `shouldBe` (42 :: Int)
+            Failure _ err <- evaluateExample $ prop $ 23 `shouldBe` (42 :: Int)
 #if MIN_VERSION_HUnit(1,5,0)
-            err `shouldBe` H.ExpectedButGot (Just "Falsifiable (after 1 test):\n  0\n  1") "42" "23"
+            err `shouldBe` ExpectedButGot (Just "Falsifiable (after 1 test):\n  0\n  1") "42" "23"
 #else
-            err `shouldBe` H.Reason "Falsifiable (after 1 test):\n  0\n  1\nexpected: 42\n but got: 23"
+            err `shouldBe` Reason "Falsifiable (after 1 test):\n  0\n  1\nexpected: 42\n but got: 23"
 #endif
 
         context "when used with assertEqual" $ do
           it "includes prefix" $ do
-            H.Failure _ err <- evaluateExample $ prop $ assertEqual "foobar" (42 :: Int) 23
+            Failure _ err <- evaluateExample $ prop $ assertEqual "foobar" (42 :: Int) 23
 #if MIN_VERSION_HUnit(1,5,0)
-            err `shouldBe` H.ExpectedButGot (Just "Falsifiable (after 1 test):\n  0\n  1\nfoobar") "42" "23"
+            err `shouldBe` ExpectedButGot (Just "Falsifiable (after 1 test):\n  0\n  1\nfoobar") "42" "23"
 #else
-            err `shouldBe` H.Reason "Falsifiable (after 1 test):\n  0\n  1\nfoobar\nexpected: 42\n but got: 23"
+            err `shouldBe` Reason "Falsifiable (after 1 test):\n  0\n  1\nfoobar\nexpected: 42\n but got: 23"
 #endif
 
         context "when used with assertFailure" $ do
           it "includes reason" $ do
-            H.Failure _ err <- evaluateExample $ prop (assertFailure "foobar" :: IO ())
-            err `shouldBe` H.Reason "Falsifiable (after 1 test):\n  0\n  1\nfoobar"
+            Failure _ err <- evaluateExample $ prop (assertFailure "foobar" :: IO ())
+            err `shouldBe` Reason "Falsifiable (after 1 test):\n  0\n  1\nfoobar"
 
       context "when used with `pending`" $ do
         it "returns Pending" $ do
           let location =
 #if MIN_VERSION_base(4,8,1)
-                Just $ H.Location __FILE__ (__LINE__ + 4) 37
+                Just $ Location __FILE__ (__LINE__ + 4) 37
 #else
                 Nothing
 #endif
-          evaluateExample (property H.pending) `shouldReturn` H.Pending location Nothing
+          evaluateExample (property H.pending) `shouldReturn` Pending location Nothing
 
       context "when used with `pendingWith`" $ do
         it "includes the optional reason" $ do
           let location =
 #if MIN_VERSION_base(4,8,1)
-                Just $ H.Location __FILE__ (__LINE__ + 4) 39
+                Just $ Location __FILE__ (__LINE__ + 4) 39
 #else
                 Nothing
 #endif
-          evaluateExample (property $ H.pendingWith "foo") `shouldReturn` H.Pending location (Just "foo")
+          evaluateExample (property $ H.pendingWith "foo") `shouldReturn` Pending location (Just "foo")
 
   describe "Expectation" $ do
     context "as a QuickCheck property" $ do
