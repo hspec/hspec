62e0c4437c7fb88179491941081257b9cdbb8096
Simon Hengel <sol@typeful.net>
2018-01-03 06:54:53 +0800

    Test.Hspec.Discover.SortSpec: Test injectivity
    In addition, remove tests for reflexivity, symmetry and transitivity, as
    these are given for derived Eq / Ord instances.

--- hspec-discover/test/Test/Hspec/Discover/SortSpec.hs
+++ hspec-discover/test/Test/Hspec/Discover/SortSpec.hs
@@ -15,34 +15,38 @@ main = hspec spec
 spec :: Spec
 spec = do
   describe "naturalSortKey" $ do
+    it "is injective" $ property $ \ a  b -> do
+      a /= b ==> naturalSortKey a /= naturalSortKey b
+
     context "compares" $ do
-      it "reflexively" $ property $ \a -> do
-        a <=> a `shouldBe` EQ
-      it "symmetrically" $ property $ \a b -> do
-        a <=> b === compare EQ (b <=> a)
-      it "transitively" $ property $ \a b c -> do
-        a <=> b == b <=> c ==> a <=> b === a <=> c
       it "empty first" $ property $ \a -> not (null a) ==> do
         "" <=> a `shouldBe` LT
         a <=> "" `shouldBe` GT
+
       it "numbers first" $ do
         "Hello2World" <=> "Hello World" `shouldBe` LT
         "Hello World" <=> "Hello2World" `shouldBe` GT
+
       it "string segments" $ do
         "Hello2World9" <=> "Hello2World!0" `shouldBe` LT
         "Hello2World!0" <=> "Hello2World9" `shouldBe` GT
+
       it "numeric segments" $ do
         "3.1.415" <=> "3.14.15" `shouldBe` LT
         "3.14.15" <=> "3.1.415" `shouldBe` GT
+
       it "numeric ties by string length" $ do
         "Hello 02 World" <=> "Hello 002 World" `shouldBe` LT
         "Hello 002 World" <=> "Hello 02 World" `shouldBe` GT
+
       it "case squashing" $ do
         "Helloworld1" <=> "HelloWorld2" `shouldBe` LT
         "HelloWorld2" <=> "helloworld1" `shouldBe` GT
+
       it "string ties by case" $ do
         "HelloWorld3" <=> "Helloworld3" `shouldBe` LT
         "Helloworld3" <=> "HelloWorld3" `shouldBe` GT
+
     context "sorts" $ do
       it "for humans" $ do
         sortBy (<=>)
@@ -52,6 +56,7 @@ spec = do
           [ "z" ++ show (n :: Int) ++ ".txt"
           | n <- [1..20] ++ [100..102]
           ]
+
   where
     (<=>) = comparing naturalSortKey
     infix 5 <=>
