11837f69e345092897d3b360164f6b2f07747225
Simon Hengel <sol@typeful.net>
2018-01-03 07:53:05 +0800

    Test.Hspec.Discover.SortSpec: Remove redundant expectations
    For derived Ord instances we can assume: a < b <=> b > a

--- hspec-discover/test/Test/Hspec/Discover/SortSpec.hs
+++ hspec-discover/test/Test/Hspec/Discover/SortSpec.hs
@@ -25,27 +25,21 @@ spec = do
 
       it "numbers first" $ do
         "Hello2World" <=> "Hello World" `shouldBe` LT
-        "Hello World" <=> "Hello2World" `shouldBe` GT
 
       it "string segments" $ do
         "Hello2World9" <=> "Hello2World!0" `shouldBe` LT
-        "Hello2World!0" <=> "Hello2World9" `shouldBe` GT
 
       it "numeric segments" $ do
         "3.1.415" <=> "3.14.15" `shouldBe` LT
-        "3.14.15" <=> "3.1.415" `shouldBe` GT
 
       it "numeric ties by string length" $ do
         "Hello 02 World" <=> "Hello 002 World" `shouldBe` LT
-        "Hello 002 World" <=> "Hello 02 World" `shouldBe` GT
 
       it "case squashing" $ do
         "Helloworld1" <=> "HelloWorld2" `shouldBe` LT
-        "HelloWorld2" <=> "helloworld1" `shouldBe` GT
 
       it "string ties by case" $ do
         "HelloWorld3" <=> "Helloworld3" `shouldBe` LT
-        "Helloworld3" <=> "HelloWorld3" `shouldBe` GT
 
     context "sorts" $ do
       it "for humans" $ do
