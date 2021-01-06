917a06b90ae0292d4fe3d019bbb8cdba226b3738
Simon Hengel <sol@typeful.net>
2021-01-06 09:56:48 +0700

    Update docs

--- doc/_includes/QuickCheckProp.hs
+++ doc/_includes/QuickCheckProp.hs
@@ -1,9 +1,9 @@
 import Test.Hspec
-import Test.QuickCheck
+import Test.Hspec.QuickCheck
 
 main :: IO ()
 main = hspec $ do
   describe "read" $ do
     context "when used with Int" $ do
-      it "is inverse to show" $ property $
+      prop "is inverse to show" $
         \x -> (read . show) x `shouldBe` (x :: Int)
