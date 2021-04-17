82e2e6c05f59c192fcd6bf4633016e96292a7b38
Simon Hengel <sol@typeful.net>
2021-04-18 04:46:44 +0700

    Remove postProcessSpec from hspec-gerated test drivers

--- hspec-discover/test/Test/Hspec/Discover/RunSpec.hs
+++ hspec-discover/test/Test/Hspec/Discover/RunSpec.hs
@@ -8,7 +8,7 @@ import           System.FilePath
 import           Data.List (sort)
 
 import           Test.Hspec.Discover.Run hiding (Spec)
-import qualified Test.Hspec.Discover.Run
+import qualified Test.Hspec.Discover.Run as Run
 
 main :: IO ()
 main = hspec spec
@@ -39,9 +39,9 @@ spec = do
         , "main = hspec spec"
         , "spec :: Spec"
         , "spec = " ++ unwords [
-               "postProcessSpec \"test-data/nested-spec/Foo/Bar/BazSpec.hs\" (describe \"Foo.Bar.Baz\" Foo.Bar.BazSpec.spec)"
-          , ">> postProcessSpec \"test-data/nested-spec/Foo/BarSpec.hs\" (describe \"Foo.Bar\" Foo.BarSpec.spec)"
-          , ">> postProcessSpec \"test-data/nested-spec/FooSpec.hs\" (describe \"Foo\" FooSpec.spec)"
+               "describe \"Foo.Bar.Baz\" Foo.Bar.BazSpec.spec"
+          , ">> describe \"Foo.Bar\" Foo.BarSpec.spec"
+          , ">> describe \"Foo\" FooSpec.spec"
           ]
         ]
 
@@ -74,37 +74,35 @@ spec = do
 
   describe "fileToSpec" $ do
     it "converts path to spec name" $ do
-      fileToSpec "" "FooSpec.hs" `shouldBe` Just (spec_ "FooSpec.hs" "Foo")
+      fileToSpec "FooSpec.hs" `shouldBe` Just (Run.Spec "Foo")
 
     it "rejects spec with empty name" $ do
-      fileToSpec "" "Spec.hs" `shouldBe` Nothing
+      fileToSpec "Spec.hs" `shouldBe` Nothing
 
     it "works for lhs files" $ do
-      fileToSpec "" "FooSpec.lhs" `shouldBe` Just (spec_ "FooSpec.lhs" "Foo")
+      fileToSpec "FooSpec.lhs" `shouldBe` Just (Run.Spec "Foo")
 
     it "returns Nothing for invalid spec name" $ do
-      fileToSpec "" "foo" `shouldBe` Nothing
+      fileToSpec "foo" `shouldBe` Nothing
 
     context "when spec does not have a valid module name" $ do
       it "returns Nothing" $ do
-        fileToSpec "" "flycheck_Spec.hs" `shouldBe` Nothing
+        fileToSpec "flycheck_Spec.hs" `shouldBe` Nothing
 
     context "when any component of a hierarchical module name is not valid"$ do
       it "returns Nothing" $ do
-        fileToSpec "" ("Valid" </> "invalid"  </>"MiddleNamesSpec.hs") `shouldBe` Nothing
+        fileToSpec ("Valid" </> "invalid"  </>"MiddleNamesSpec.hs") `shouldBe` Nothing
 
     context "when path has directory component" $ do
       it "converts path to spec name" $ do
-        let file = "Foo" </> "Bar" </> "BazSpec.hs"
-        fileToSpec "" file `shouldBe` Just (spec_ file "Foo.Bar.Baz")
+        fileToSpec ("Foo" </> "Bar" </> "BazSpec.hs") `shouldBe` Just (Run.Spec "Foo.Bar.Baz")
 
       it "rejects spec with empty name" $ do
-        fileToSpec "" ("Foo" </> "Bar" </> "Spec.hs") `shouldBe` Nothing
+        fileToSpec ("Foo" </> "Bar" </> "Spec.hs") `shouldBe` Nothing
 
   describe "findSpecs" $ do
     it "finds specs" $ do
-      let dir = "test-data/nested-spec"
-      findSpecs (dir </> "Spec.hs") `shouldReturn` [spec_ (dir </> "Foo/Bar/BazSpec.hs") "Foo.Bar.Baz", spec_ (dir </> "Foo/BarSpec.hs") "Foo.Bar", spec_ (dir </> "FooSpec.hs") "Foo"]
+      findSpecs ("test-data" </> "nested-spec" </> "Spec.hs") `shouldReturn` [Run.Spec "Foo.Bar.Baz", Run.Spec "Foo.Bar", Run.Spec "Foo"]
 
   describe "driverWithFormatter" $ do
     it "generates a test driver that uses a custom formatter" $ do
@@ -120,9 +118,7 @@ spec = do
 
   describe "importList" $ do
     it "generates imports for a list of specs" $ do
-      importList [spec_ "FooSpec.hs" "Foo", spec_ "BarSpec.hs" "Bar"] "" `shouldBe` unlines [
+      importList [Run.Spec "Foo", Run.Spec "Bar"] "" `shouldBe` unlines [
           "import qualified FooSpec"
         , "import qualified BarSpec"
         ]
-  where
-    spec_ = Test.Hspec.Discover.Run.Spec
