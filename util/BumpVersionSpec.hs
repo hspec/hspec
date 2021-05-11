8ce7d2df846eb74f94f55587fab2cfa8abf81e92
Simon Hengel <sol@typeful.net>
2021-05-11 22:24:03 +0700

    Release 2.8.0

--- util/BumpVersionSpec.hs
+++ util/BumpVersionSpec.hs
@@ -7,5 +7,8 @@ import           BumpVersion
 spec :: Spec
 spec = do
   describe "bumpVersion" $ do
-    it "bumps version" $ do
-      bumpVersion "&version 2.7.2\n" `shouldBe` "&version 2.7.3\n"
+    it "bumps minor version" $ do
+      bumpVersion Minor "&version 2.7.2\n" `shouldBe` "&version 2.7.3\n"
+
+    it "bumps major version" $ do
+      bumpVersion Major "&version 2.7.2\n" `shouldBe` "&version 2.8.0\n"
