bc36a75810c18898de735c19290cc249c4a06f94
Simon Hengel <sol@typeful.net>
2021-11-23 15:09:58 +0700

    XXX drop

--- hspec-core/example/Spec.hs
+++ hspec-core/example/Spec.hs
@@ -15,5 +15,14 @@ spec = do
     it "reverses a list" $ do
       reverse [1 :: Int, 2, 3] `shouldBe` [3, 2, 1]
 
+    it "reverses a list" $ do
+      reverse [1 :: Int, 2, 3] `shouldBe` [2, 1]
+
     it "gives the original list, if applied twice" $ property $
       \xs -> (reverse . reverse) xs == (xs :: [Int])
+
+    it "gives the original list, if applied twice" $ property $
+      pending
+
+    it "reverses a list" $ do
+      "foo\nbar\nbaz  \n" `shouldBe` "foo  \nbar\nbaz\n"
