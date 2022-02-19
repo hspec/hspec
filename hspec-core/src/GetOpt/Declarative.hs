abe6d93e78db64636abd34b654f6fa569ccb92da
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-19 20:15:27 +0800

    Delete hspec-core directory

--- hspec-core/src/GetOpt/Declarative.hs
+++
@@ -1,5 +0,0 @@
-module GetOpt.Declarative (module GetOpt.Declarative) where
-import           Prelude ()
-import           GetOpt.Declarative.Types as GetOpt.Declarative
-import           GetOpt.Declarative.Interpret as GetOpt.Declarative
-import           GetOpt.Declarative.Environment as GetOpt.Declarative
--- hspec-core/src/Test/Hspec/Core/Formatters.hs
+++
@@ -1,2 +0,0 @@
-module Test.Hspec.Core.Formatters (module V1) where
-import           Test.Hspec.Core.Formatters.V1 as V1
