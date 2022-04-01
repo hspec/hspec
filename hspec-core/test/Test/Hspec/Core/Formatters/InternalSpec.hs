323ea8b894d228e6250d6835ab57c40e4484231f
Simon Hengel <sol@typeful.net>
2022-04-02 03:40:45 +0700

    Simplify tests

--- hspec-core/test/Test/Hspec/Core/Formatters/InternalSpec.hs
+++ hspec-core/test/Test/Hspec/Core/Formatters/InternalSpec.hs
@@ -3,10 +3,36 @@ module Test.Hspec.Core.Formatters.InternalSpec (spec) where
 import           Prelude ()
 import           Helper
 
+import           System.Console.ANSI
+
+import           Test.Hspec.Core.Format
 import           Test.Hspec.Core.Formatters.Internal
+
+formatConfig :: FormatConfig
+formatConfig = FormatConfig {
+  formatConfigUseColor = True
+, formatConfigReportProgress = False
+, formatConfigOutputUnicode = False
+, formatConfigUseDiff = False
+, formatConfigPrettyPrint = False
+, formatConfigPrintTimes = False
+, formatConfigHtmlOutput = False
+, formatConfigPrintCpuTime = False
+, formatConfigUsedSeed = 0
+, formatConfigExpectedTotalCount = 0
+}
+
+green :: String -> String
+green text = setSGRCode [SetColor Foreground Dull Green] <> text <> setSGRCode [Reset]
 
 spec :: Spec
 spec = do
+  describe "write" $ do
+    it "does not span colored output over multiple lines" $ do
+      capture_ $ runFormatM formatConfig $ do
+        withSuccessColor $ write "foo\nbar\nbaz\n"
+      `shouldReturn` unlines [green "foo", green "bar", green "baz"]
+
   describe "overwriteWith" $ do
     context "when old is null" $ do
       it "returns new" $ do
