d4b2301f3398d22ef56b2f99bf85356e66188898
Simon Hengel <sol@typeful.net>
2022-04-11 02:48:05 +0700

    Handle pretty-printing in `Test.Hspec.Core.Runner.Eval`
    (instead of `Test.Hspec.Core.Formatters.V2`)

--- hspec-core/test/Test/Hspec/Core/Formatters/V2Spec.hs
+++ hspec-core/test/Test/Hspec/Core/Formatters/V2Spec.hs
@@ -204,25 +204,6 @@ spec = do
           ]
 
     describe "formatterDone" $ do
-      it "recovers unicode from ExpectedButGot" $ do
-        formatter <- formatterToFormat failed_examples formatConfig { formatConfigOutputUnicode = True }
-        _ <- formatter .  ItemDone ([], "") . Item Nothing 0 "" $ Failure Nothing $ ExpectedButGot Nothing (show "\955") (show "\956")
-        (fmap normalizeSummary . captureLines) (formatter $ Done []) `shouldReturn` [
-            ""
-          , "Failures:"
-          , ""
-          , "  1) "
-          , "       expected: \"λ\""
-          , "        but got: \"μ\""
-          , ""
-          , "  To rerun use: --match \"//\""
-          , ""
-          , "Randomized with seed 0"
-          , ""
-          , "Finished in 0.0000 seconds"
-          , "1 example, 1 failure"
-          ]
-
       context "when actual/expected contain newlines" $ do
         it "adds indentation" $ do
           formatter <- formatterToFormat failed_examples formatConfig
