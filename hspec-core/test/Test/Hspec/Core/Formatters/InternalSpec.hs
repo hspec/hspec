hspec/hspec

commit 0d963317c5401b940bfd3fa6903b1f5ed33d6eb1

Do not colorize whitespace-only output

hspec-core/src/Test/Hspec/Core/Formatters/Internal.hs
@@ -230,12 +230,12 @@ write str = do
   h <- getHandle
   mColor <- gets stateColor
   liftIO $ case mColor of
-    Just color ->
+    Just color | not (all isSpace str) ->
       doLines
         (\l -> bracket_ (hSetSGR h [color]) (hSetSGR h [Reset]) (IO.hPutStr h l))
         (IO.hPutStr h "\n")
         str
-    Nothing -> IO.hPutStr h str
+    _ -> IO.hPutStr h str
   where
     doLines :: (String -> IO ()) -> IO () -> String -> IO ()
     doLines handleLine handleNewline s = do

commit e02a4c4d720c2d9ccc21471990e4ded9aef94f5e

Make sure that whitespace-only diff output is colorized properly
This fixes a regression that was introduced in
0d963317c5401b940bfd3fa6903b1f5ed33d6eb1.

(close #660)

hspec-core/hspec-core.cabal
@@ -5,7 +5,7 @@ cabal-version: 1.12
 -- see: https://github.com/sol/hpack
 
 name:             hspec-core
-version:          2.10.3
+version:          2.10.4
 license:          MIT
 license-file:     LICENSE
 copyright:        (c) 2011-2022 Simon Hengel,

hspec-discover/hspec-discover.cabal
@@ -5,7 +5,7 @@ cabal-version: 1.12
 -- see: https://github.com/sol/hpack
 
 name:             hspec-discover
-version:          2.10.3
+version:          2.10.4
 license:          MIT
 license-file:     LICENSE
 copyright:        (c) 2012-2022 Simon Hengel

hspec.cabal
@@ -5,7 +5,7 @@ cabal-version: 1.12
 -- see: https://github.com/sol/hpack
 
 name:             hspec
-version:          2.10.3
+version:          2.10.4
 license:          MIT
 license-file:     LICENSE
 copyright:        (c) 2011-2022 Simon Hengel,
@@ -45,8 +45,8 @@ library
   build-depends:
       QuickCheck >=2.12
     , base ==4.*
-    , hspec-core ==2.10.3
-    , hspec-discover ==2.10.3
+    , hspec-core ==2.10.4
+    , hspec-discover ==2.10.4
     , hspec-expectations ==0.8.2.*
   exposed-modules:
       Test.Hspec

version.yaml
@@ -1,1 +1,1 @@
-&version 2.10.3
+&version 2.10.4

hspec-core/src/Test/Hspec/Core/Formatters/Internal.hs
@@ -243,13 +243,14 @@ splitLines = groupBy (\ a b -> isNewline a == isNewline b)
 writeChunk :: String -> FormatM ()
 writeChunk str = do
   h <- getHandle
+  let
+    plainOutput = IO.hPutStr h str
+    colorOutput color = bracket_ (hSetSGR h [color]) (hSetSGR h [Reset]) plainOutput
   mColor <- gets stateColor
   liftIO $ case mColor of
-    Just color | not (all isSpace str) -> bracket_
-      (hSetSGR h [color])
-      (hSetSGR h [Reset])
-      (IO.hPutStr h str)
-    _ -> IO.hPutStr h str
+    Just (SetColor Foreground _ _) | all isSpace str -> plainOutput
+    Just color -> colorOutput color
+    Nothing -> plainOutput
 
 -- | Set output color to red, run given action, and finally restore the default
 -- color.

hspec-core/test/Test/Hspec/Core/Formatters/InternalSpec.hs
@@ -13,7 +13,7 @@ formatConfig = FormatConfig {
   formatConfigUseColor = True
 , formatConfigReportProgress = False
 , formatConfigOutputUnicode = False
-, formatConfigUseDiff = False
+, formatConfigUseDiff = True
 , formatConfigPrettyPrint = False
 , formatConfigPrettyPrintFunction = Nothing
 , formatConfigPrintTimes = False
@@ -28,8 +28,37 @@ green text = setSGRCode [SetColor Foreground Dull Green] <> text <> setSGRCode [
 
 spec :: Spec
 spec = do
+  forM_ [
+      ("extraChunk", extraChunk, Red)
+    , ("missingChunk", missingChunk, Green)
+    ] $ \ (name, chunk, color) -> do
+
+    let colorize layer text = setSGRCode [SetColor layer Dull color] <> text <> setSGRCode [Reset]
+
+    describe name $ do
+      it "colorizes chunks" $ do
+        capture_ $ runFormatM formatConfig $ do
+          chunk "foo"
+        `shouldReturn` colorize Foreground "foo"
+
+      context "with an all-spaces chunk" $ do
+        it "colorizes background" $ do
+          capture_ $ runFormatM formatConfig $ do
+            chunk "  "
+          `shouldReturn` colorize Background "  "
+
+      context "with an all-newlines chunk" $ do
+        it "colorizes background" $ do
+          capture_ $ runFormatM formatConfig $ do
+            chunk "\n\n\n"
+          `shouldReturn` colorize Background "\n\n\n"
+
   describe "write" $ do
     it "does not span colored output over multiple lines" $ do
+
+      -- This helps with output on Jenkins and Buildkite:
+      -- https://github.com/hspec/hspec/issues/346
+
       capture_ $ runFormatM formatConfig $ do
         withSuccessColor $ write "foo\nbar\nbaz\n"
       `shouldReturn` unlines [green "foo", green "bar", green "baz"]