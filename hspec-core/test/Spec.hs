8f0c24797b2dc3a5db711aadaa265a5cc9a43cc7
Simon Hengel <sol@typeful.net>
2022-09-13 10:16:23 +0700

    Don't use `--module-name` for spec discovery
    Use `test/SpecHook.hs` intead.

--- hspec-core/test/Spec.hs
+++ hspec-core/test/Spec.hs
@@ -1,13 +1,1 @@
-module Main where
-
-import           Prelude ()
-import           Helper
-
-import           Test.Hspec.Meta
-import qualified All
-
-spec :: Spec
-spec = aroundAll_ (withEnvironment [("IGNORE_DOT_HSPEC", "yes")]) All.spec
-
-main :: IO ()
-main = hspec spec
+{-# OPTIONS_GHC -fforce-recomp -F -pgmF hspec-meta-discover #-}
