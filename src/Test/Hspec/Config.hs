3eb31d92aa685cd9efd0135361f4398eb781f9f5
Simon Hengel <sol@typeful.net>
2022-12-25 16:47:58 +0700

    Add Test.Hspec.Config

---
+++ src/Test/Hspec/Config.hs
@@ -0,0 +1,15 @@
+-- |
+-- Stability: provisional
+--
+-- @since 2.10.9
+module Test.Hspec.Config (
+  Config(..)
+, ColorMode(..)
+, UnicodeMode(..)
+, Path
+, defaultConfig
+, modifyConfig
+) where
+
+import Test.Hspec.Core.Spec
+import Test.Hspec.Core.Runner
