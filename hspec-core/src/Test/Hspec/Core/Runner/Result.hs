c4638454917d492148518d2b8b40b6988f7171d3
Simon Hengel <sol@typeful.net>
2023-01-28 18:09:59 +0700

    Move `Summary` to `Test.Hspec.Core.Runner.Result`
    (from `Test.Hspec.Core.Runner`)

--- hspec-core/src/Test/Hspec/Core/Runner/Result.hs
+++ hspec-core/src/Test/Hspec/Core/Runner/Result.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 module Test.Hspec.Core.Runner.Result (
 -- RE-EXPORTED from Test.Hspec.Core.Runner
   SpecResult(SpecResult)
@@ -10,6 +11,10 @@ module Test.Hspec.Core.Runner.Result (
 , resultItemIsFailure
 
 , ResultItemStatus(..)
+
+, Summary(..)
+, toSummary
+, isSuccess
 -- END RE-EXPORTED from Test.Hspec.Core.Runner
 
 , toSpecResult
@@ -72,3 +77,35 @@ toResultItem (path, item) = ResultItem path status
       Format.Success{} -> ResultItemSuccess
       Format.Pending{} -> ResultItemPending
       Format.Failure{} -> ResultItemFailure
+
+-- | Summary of a test run.
+data Summary = Summary {
+  summaryExamples :: !Int
+, summaryFailures :: !Int
+} deriving (Eq, Show)
+
+instance Monoid Summary where
+  mempty = Summary 0 0
+#if MIN_VERSION_base(4,11,0)
+instance Semigroup Summary where
+#endif
+  (Summary x1 x2)
+#if MIN_VERSION_base(4,11,0)
+    <>
+#else
+    `mappend`
+#endif
+    (Summary y1 y2) = Summary (x1 + y1) (x2 + y2)
+
+toSummary :: SpecResult -> Summary
+toSummary result = Summary {
+  summaryExamples = length items
+, summaryFailures = length failures
+} where
+    items = specResultItems result
+    failures = filter resultItemIsFailure items
+
+-- | `True` if the given `Summary` indicates that there were no
+-- failures, `False` otherwise.
+isSuccess :: Summary -> Bool
+isSuccess summary = summaryFailures summary == 0
