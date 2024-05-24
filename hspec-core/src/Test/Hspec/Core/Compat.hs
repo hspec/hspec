c19f465d082b585e21f4e0d9f62691bdc9b0792a
Simon Hengel <sol@typeful.net>
2024-05-24 11:54:33 +0700

    Compatibility with GHC 9.10.1

--- hspec-core/src/Test/Hspec/Core/Compat.hs
+++ hspec-core/src/Test/Hspec/Core/Compat.hs
@@ -18,7 +18,7 @@ import           Control.Monad as Imports hiding (
   )
 import           Data.Maybe as Imports
 import           Data.Foldable as Imports
-import           Data.CallStack as Imports (HasCallStack)
+import           GHC.Stack as Imports (HasCallStack, withFrozenCallStack)
 
 import           System.IO
 import           System.Exit
@@ -74,75 +74,13 @@ import           GHC.ResponseFile as Imports (unescapeArgs)
 import           Data.Char
 #endif
 
-#if MIN_VERSION_base(4,6,0)
 import           Text.Read as Imports (readMaybe)
 import           System.Environment as Imports (lookupEnv)
-#else
-import           Text.Read
-import qualified Text.ParserCombinators.ReadP as P
-#endif
 
-#if !MIN_VERSION_base(4,8,0)
-import           Data.Ord (comparing)
-#endif
 
-#if MIN_VERSION_base(4,7,0)
 import           Data.Bool as Imports (bool)
-#endif
 
 import           Control.Concurrent
-
-#if !MIN_VERSION_base(4,9,0)
-import           GHC.IO
-#endif
-
-#if !MIN_VERSION_base(4,6,0)
-forkFinally :: IO a -> (Either SomeException a -> IO ()) -> IO ThreadId
-forkFinally action and_then =
-  mask $ \restore ->
-    forkIO $ try (restore action) >>= and_then
-
--- |Strict version of 'modifyIORef'
-modifyIORef' :: IORef a -> (a -> a) -> IO ()
-modifyIORef' ref f = do
-    x <- readIORef ref
-    let x' = f x
-    x' `seq` writeIORef ref x'
-
-atomicWriteIORef :: IORef a -> a -> IO ()
-atomicWriteIORef ref a = do
-    x <- atomicModifyIORef ref (\_ -> (a, ()))
-    x `seq` pass
-
--- | Parse a string using the 'Read' instance.
--- Succeeds if there is exactly one valid result.
--- A 'Left' value indicates a parse error.
-readEither :: Read a => String -> Either String a
-readEither s =
-  case [ x | (x,"") <- readPrec_to_S read' minPrec s ] of
-    [x] -> Right x
-    []  -> Left "Prelude.read: no parse"
-    _   -> Left "Prelude.read: ambiguous parse"
- where
-  read' =
-    do x <- readPrec
-       lift P.skipSpaces
-       return x
-
--- | Parse a string using the 'Read' instance.
--- Succeeds if there is exactly one valid result.
-readMaybe :: Read a => String -> Maybe a
-readMaybe s = case readEither s of
-                Left _  -> Nothing
-                Right a -> Just a
-
--- | Return the value of the environment variable @var@, or @Nothing@ if
--- there is no such value.
---
--- For POSIX users, this is equivalent to 'System.Posix.Env.getEnv'.
-lookupEnv :: String -> IO (Maybe String)
-lookupEnv k = lookup k `fmap` getEnvironment
-#endif
 
 showType :: Typeable a => a -> String
 showType a = let t = typeRepTyCon (typeOf a) in
@@ -154,16 +92,6 @@ showFullType a = let t = typeRepTyCon (typeOf a) in
 
 getDefaultConcurrentJobs :: IO Int
 getDefaultConcurrentJobs = getNumCapabilities
-
-#if !MIN_VERSION_base(4,9,0)
-interruptible :: IO a -> IO a
-interruptible act = do
-  st <- getMaskingState
-  case st of
-    Unmasked              -> act
-    MaskedInterruptible   -> unsafeUnmask act
-    MaskedUninterruptible -> act
-#endif
 
 guarded :: Alternative m => (a -> Bool) -> a -> m a
 guarded p a = if p a then pure a else empty
