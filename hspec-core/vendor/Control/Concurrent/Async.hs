8858773da365034e4ff40b3906bee8b39e288b3d
Simon Hengel <sol@typeful.net>
2025-12-09 13:32:05 +0700

    Update to `async-2.2.5`

--- hspec-core/vendor/Control/Concurrent/Async.hs
+++ hspec-core/vendor/Control/Concurrent/Async.hs
@@ -10,7 +10,7 @@
 
 -----------------------------------------------------------------------------
 -- |
--- Module      :  Control.Concurrent.Async
+-- Module      :  Control.Concurrent.Async.Internal
 -- Copyright   :  (c) Simon Marlow 2012
 -- License     :  BSD3 (see the file LICENSE)
 --
@@ -18,182 +18,14 @@
 -- Stability   :  provisional
 -- Portability :  non-portable (requires concurrency)
 --
--- This module provides a set of operations for running IO operations
--- asynchronously and waiting for their results.  It is a thin layer
--- over the basic concurrency operations provided by
--- "Control.Concurrent".  The main additional functionality it
--- provides is the ability to wait for the return value of a thread,
--- but the interface also provides some additional safety and
--- robustness over using 'forkIO' threads and @MVar@ directly.
+-- This module is an internal module. The public API is provided in
+-- "Control.Concurrent.Async". Breaking changes to this module will not be
+-- reflected in a major bump, and using this module may break your code
+-- unless you are extremely careful.
 --
--- == High-level API
---
--- @async@'s high-level API spawns /lexically scoped/ threads,
--- ensuring the following key poperties that make it safer to use
--- than using plain 'forkIO':
---
--- 1. No exception is swallowed (waiting for results propagates exceptions).
--- 2. No thread is leaked (left running unintentionally).
---
--- (This is done using the 'Control.Exception.bracket' pattern to work in presence
--- of synchornous and asynchronous exceptions.)
---
--- __Most practical/production code should only use the high-level API__.
---
--- The basic type is @'Async' a@, which represents an asynchronous
--- @IO@ action that will return a value of type @a@, or die with an
--- exception.  An 'Async' is a wrapper around a low-level 'forkIO' thread.
---
--- The fundamental function to spawn threads with the high-level API is
--- 'withAsync'.
---
--- For example, to fetch two web pages at the same time, we could do
--- this (assuming a suitable @getURL@ function):
---
--- > withAsync (getURL url1) $ \a1 -> do
--- >   withAsync (getURL url2) $ \a2 -> do
--- >     page1 <- wait a1
--- >     page2 <- wait a2
--- >     ...
---
--- where 'withAsync' starts the operation in a separate thread, and
--- 'wait' waits for and returns the result.
---
--- * If the operation throws an exception, then that exception is re-thrown
---   by 'wait'. This ensures property (1): No exception is swallowed.
--- * If an exception bubbles up through a 'withAsync', then the 'Async'
---   it spawned is 'cancel'ed. This ensures property (2): No thread is leaked.
---
--- Often we do not care to work manually with 'Async' handles like
--- @a1@ and @a2@. Instead, we want to express high-level objectives like
--- performing two or more tasks concurrently, and waiting for one or all
--- of them to finish.
---
--- For example, the pattern of performing two IO actions concurrently and
--- waiting for both their results is packaged up in a combinator 'concurrently',
--- so we can further shorten the above example to:
---
--- > (page1, page2) <- concurrently (getURL url1) (getURL url2)
--- > ...
---
--- The section __/High-level utilities/__ covers the most
--- common high-level objectives, including:
---
--- * Waiting for 2 results ('concurrently').
--- * Waiting for many results ('mapConcurrently' / 'forConcurrently').
--- * Waiting for the first of 2 results ('race').
--- * Waiting for arbitrary nestings of "all of /N/" and "the first of /N/"
---   results with the 'Concurrently' newtype and its 'Applicative' and
---   'Alternative' instances.
---
--- Click here to scroll to that section:
--- "Control.Concurrent.Async#high-level-utilities".
---
--- == Low-level API
---
--- Some use cases require parallelism that is not lexically scoped.
---
--- For those, the low-level function 'async' can be used as a direct
--- equivalent of 'forkIO':
---
--- > -- Do NOT use this code in production, it has a flaw (explained below).
--- > do
--- >   a1 <- async (getURL url1)
--- >   a2 <- async (getURL url2)
--- >   page1 <- wait a1
--- >   page2 <- wait a2
--- >   ...
---
--- In contrast to 'withAsync', this code has a problem.
---
--- It still fulfills property (1) in that an exception arising from
--- @getUrl@ will be re-thrown by 'wait', but it does not fulfill
--- property (2).
--- Consider the case when the first 'wait' throws an exception; then the
--- second 'wait' will not happen, and the second 'async' may be left
--- running in the background, possibly indefinitely.
---
--- 'withAsync' is like 'async', except that the 'Async' is
--- automatically killed (using 'uninterruptibleCancel') if the
--- enclosing IO operation returns before it has completed.
--- Furthermore, 'withAsync' allows a tree of threads to be built, such
--- that children are automatically killed if their parents die for any
--- reason.
---
--- If you need to use the low-level API, ensure that you gurantee
--- property (2) by other means, such as 'link'ing asyncs that need
--- to die together, and protecting against asynchronous exceptions
--- using 'Control.Exception.bracket', 'Control.Exception.mask',
--- or other functions from "Control.Exception".
---
--- == Miscellaneous
---
--- The 'Functor' instance can be used to change the result of an
--- 'Async'.  For example:
---
--- > ghci> withAsync (return 3) (\a -> wait (fmap (+1) a))
--- > 4
---
--- === Resource exhaustion
---
--- As with all concurrent programming, keep in mind that while
--- Haskell's cooperative ("green") multithreading carries low overhead,
--- spawning too many of them at the same time may lead to resource exhaustion
--- (of memory, file descriptors, or other limited resources), given that the
--- actions running in the threads consume these resources.
-
 -----------------------------------------------------------------------------
 
-module Control.Concurrent.Async (
-
-    -- * Asynchronous actions
-    Async,
-
-    -- * High-level API
-
-    -- ** Spawning with automatic 'cancel'ation
-    withAsync, withAsyncBound, withAsyncOn, withAsyncWithUnmask,
-    withAsyncOnWithUnmask,
-
-    -- ** Querying 'Async's
-    wait, poll, waitCatch, asyncThreadId,
-    cancel, uninterruptibleCancel, cancelWith, AsyncCancelled(..),
-
-    -- ** #high-level-utilities# High-level utilities
-    race, race_,
-    concurrently, concurrently_,
-    mapConcurrently, forConcurrently,
-    mapConcurrently_, forConcurrently_,
-    replicateConcurrently, replicateConcurrently_,
-    Concurrently(..),
-    compareAsyncs,
-
-    -- ** Specialised operations
-
-    -- *** STM operations
-    waitSTM, pollSTM, waitCatchSTM,
-
-    -- *** Waiting for multiple 'Async's
-    waitAny, waitAnyCatch, waitAnyCancel, waitAnyCatchCancel,
-    waitEither, waitEitherCatch, waitEitherCancel, waitEitherCatchCancel,
-    waitEither_,
-    waitBoth,
-
-    -- *** Waiting for multiple 'Async's in STM
-    waitAnySTM, waitAnyCatchSTM,
-    waitEitherSTM, waitEitherCatchSTM,
-    waitEitherSTM_,
-    waitBothSTM,
-
-    -- * Low-level API
-
-    -- ** Spawning (low-level API)
-    async, asyncBound, asyncOn, asyncWithUnmask, asyncOnWithUnmask,
-
-    -- ** Linking
-    link, linkOnly, link2, link2Only, ExceptionInLinkedThread(..),
-
-  ) where
+module Control.Concurrent.Async where
 
 import Control.Concurrent.STM.TMVar
 import Control.Exception
@@ -210,6 +42,9 @@ import Data.Traversable
 #endif
 #if __GLASGOW_HASKELL__ < 710
 import Data.Typeable
+#endif
+#if MIN_VERSION_base(4,8,0)
+import Data.Bifunctor
 #endif
 #if MIN_VERSION_base(4,9,0)
 import Data.Semigroup (Semigroup((<>)))
@@ -252,7 +87,7 @@ compareAsyncs (Async t1 _) (Async t2 _) = compare t1 t2
 
 -- | Spawn an asynchronous action in a separate thread.
 --
--- Like for 'forkIO', the action may be left running unintentinally
+-- Like for 'forkIO', the action may be left running unintentionally
 -- (see module-level documentation for details).
 --
 -- __Use 'withAsync' style functions wherever you can instead!__
@@ -423,6 +258,14 @@ pollSTM (Async _ w) = (Just <$> w) `orElse` return Nothing
 cancel :: Async a -> IO ()
 cancel a@(Async t _) = throwTo t AsyncCancelled <* waitCatch a
 
+-- | Cancel multiple asynchronous actions by throwing the @AsyncCancelled@
+-- exception to each of them in turn, then waiting for all the `Async` threads
+-- to complete.
+cancelMany :: [Async a] -> IO ()
+cancelMany as = do
+  mapM_ (\(Async t _) -> throwTo t AsyncCancelled) as
+  mapM_ waitCatch as
+
 -- | The exception thrown by `cancel` to terminate a thread.
 data AsyncCancelled = AsyncCancelled
   deriving (Show, Eq
@@ -457,6 +300,7 @@ cancelWith a@(Async t _) e = throwTo t e <* waitCatch a
 -- | Wait for any of the supplied asynchronous operations to complete.
 -- The value returned is a pair of the 'Async' that completed, and the
 -- result that would be returned by 'wait' on that 'Async'.
+-- The input list must be non-empty.
 --
 -- If multiple 'Async's complete or have completed, then the value
 -- returned corresponds to the first completed 'Async' in the list.
@@ -469,6 +313,9 @@ waitAnyCatch = atomically . waitAnyCatchSTM
 --
 -- @since 2.1.0
 waitAnyCatchSTM :: [Async a] -> STM (Async a, Either SomeException a)
+waitAnyCatchSTM [] =
+    throwSTM $ ErrorCall
+      "waitAnyCatchSTM: invalid argument: input list must be non-empty"
 waitAnyCatchSTM asyncs =
     foldr orElse retry $
       map (\a -> do r <- waitCatchSTM a; return (a, r)) asyncs
@@ -478,11 +325,12 @@ waitAnyCatchSTM asyncs =
 --
 waitAnyCatchCancel :: [Async a] -> IO (Async a, Either SomeException a)
 waitAnyCatchCancel asyncs =
-  waitAnyCatch asyncs `finally` mapM_ cancel asyncs
+  waitAnyCatch asyncs `finally` cancelMany asyncs
 
 -- | Wait for any of the supplied @Async@s to complete.  If the first
 -- to complete throws an exception, then that exception is re-thrown
 -- by 'waitAny'.
+-- The input list must be non-empty.
 --
 -- If multiple 'Async's complete or have completed, then the value
 -- returned corresponds to the first completed 'Async' in the list.
@@ -495,6 +343,9 @@ waitAny = atomically . waitAnySTM
 --
 -- @since 2.1.0
 waitAnySTM :: [Async a] -> STM (Async a, a)
+waitAnySTM [] =
+    throwSTM $ ErrorCall
+      "waitAnySTM: invalid argument: input list must be non-empty"
 waitAnySTM asyncs =
     foldr orElse retry $
       map (\a -> do r <- waitSTM a; return (a, r)) asyncs
@@ -504,7 +355,7 @@ waitAnySTM asyncs =
 --
 waitAnyCancel :: [Async a] -> IO (Async a, a)
 waitAnyCancel asyncs =
-  waitAny asyncs `finally` mapM_ cancel asyncs
+  waitAny asyncs `finally` cancelMany asyncs
 
 -- | Wait for the first of two @Async@s to finish.
 {-# INLINE waitEitherCatch #-}
@@ -535,7 +386,7 @@ waitEitherCatchCancel :: Async a -> Async b
                       -> IO (Either (Either SomeException a)
                                     (Either SomeException b))
 waitEitherCatchCancel left right =
-  waitEitherCatch left right `finally` (cancel left >> cancel right)
+  waitEitherCatch left right `finally` cancelMany [() <$ left, () <$ right]
 
 -- | Wait for the first of two @Async@s to finish.  If the @Async@
 -- that finished first raised an exception, then the exception is
@@ -574,7 +425,7 @@ waitEitherSTM_ left right =
 --
 waitEitherCancel :: Async a -> Async b -> IO (Either a b)
 waitEitherCancel left right =
-  waitEither left right `finally` (cancel left >> cancel right)
+  waitEither left right `finally` cancelMany [() <$ left, () <$ right]
 
 -- | Waits for both @Async@s to finish, but if either of them throws
 -- an exception before they have both finished, then the exception is
@@ -705,6 +556,7 @@ race :: IO a -> IO b -> IO (Either a b)
 --
 race_ :: IO a -> IO b -> IO ()
 
+
 -- | Run two @IO@ actions concurrently, and return both results.  If
 -- either action throws an exception at any time, then the other
 -- action is 'cancel'led, and the exception is re-thrown by
@@ -715,6 +567,13 @@ race_ :: IO a -> IO b -> IO ()
 -- >   withAsync right $ \b ->
 -- >   waitBoth a b
 concurrently :: IO a -> IO b -> IO (a,b)
+
+
+-- | Run two @IO@ actions concurrently. If both of them end with @Right@,
+-- return both results.  If one of then ends with @Left@, interrupt the other
+-- action and return the @Left@.
+--
+concurrentlyE :: IO (Either e a) -> IO (Either e b) -> IO (Either e (a, b))
 
 -- | 'concurrently', but ignore the result values
 --
@@ -761,6 +620,19 @@ concurrently left right = concurrently' left right (collect [])
   where
     collect [Left a, Right b] _ = return (a,b)
     collect [Right b, Left a] _ = return (a,b)
+    collect xs m = do
+        e <- m
+        case e of
+            Left ex -> throwIO ex
+            Right r -> collect (r:xs) m
+
+-- concurrentlyE :: IO (Either e a) -> IO (Either e b) -> IO (Either e (a, b))
+concurrentlyE left right = concurrently' left right (collect [])
+  where
+    collect [Left (Right a), Right (Right b)] _ = return $ Right (a,b)
+    collect [Right (Right b), Left (Right a)] _ = return $ Right (a,b)
+    collect (Left (Left ea):_) _ = return $ Left ea
+    collect (Right (Left eb):_) _ = return $ Left eb
     collect xs m = do
         e <- m
         case e of
@@ -906,6 +778,7 @@ instance Applicative Concurrently where
   Concurrently fs <*> Concurrently as =
     Concurrently $ (\(f, a) -> f a) <$> concurrently fs as
 
+-- | 'Control.Alternative.empty' waits forever. 'Control.Alternative.<|>' returns the first to finish and 'cancel's the other.
 instance Alternative Concurrently where
   empty = Concurrently $ forever (threadDelay maxBound)
   Concurrently as <|> Concurrently bs =
@@ -927,6 +800,40 @@ instance (Semigroup a, Monoid a) => Monoid (Concurrently a) where
 instance Monoid a => Monoid (Concurrently a) where
   mempty = pure mempty
   mappend = liftA2 mappend
+#endif
+
+-- | A value of type @ConcurrentlyE e a@ is an @IO@ operation that can be
+-- composed with other @ConcurrentlyE@ values, using the @Applicative@ instance.
+--
+-- Calling @runConcurrentlyE@ on a value of type @ConcurrentlyE e a@ will
+-- execute the @IO@ operations it contains concurrently, before delivering
+-- either the result of type @a@, or an error of type @e@ if one of the actions
+-- returns @Left@.
+--
+-- | @since 2.2.5
+newtype ConcurrentlyE e a = ConcurrentlyE { runConcurrentlyE :: IO (Either e a) }
+
+instance Functor (ConcurrentlyE e) where
+  fmap f (ConcurrentlyE ea) = ConcurrentlyE $ fmap (fmap f) ea
+
+#if MIN_VERSION_base(4,8,0)
+instance Bifunctor ConcurrentlyE where
+  bimap f g (ConcurrentlyE ea) = ConcurrentlyE $ fmap (bimap f g) ea
+#endif
+
+instance Applicative (ConcurrentlyE e) where
+  pure = ConcurrentlyE . return . return
+  ConcurrentlyE fs <*> ConcurrentlyE eas =
+    ConcurrentlyE $ fmap (\(f, a) -> f a) <$> concurrentlyE fs eas
+
+#if MIN_VERSION_base(4,9,0)
+-- | Either the combination of the successful results, or the first failure.
+instance Semigroup a => Semigroup (ConcurrentlyE e a) where
+  (<>) = liftA2 (<>)
+
+instance (Semigroup a, Monoid a) => Monoid (ConcurrentlyE e a) where
+  mempty = pure mempty
+  mappend = (<>)
 #endif
 
 -- ----------------------------------------------------------------------------
